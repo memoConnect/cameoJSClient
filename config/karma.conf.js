2fb655bfd22ffe301ef8b621b47c3c6ba01ca708
Michael Merz <michael.merz@indarium.de>
2014-01-30 22:12:14 +0100

    added functional tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,37 +1,75 @@
-module.exports = function(config){
+// Karma configuration
+// Generated on Thu Jan 30 2014 20:39:59 GMT+0100 (CET)
+
+module.exports = function(config) {
   config.set({
-    basePath : '../',
 
-    files : [
-      'app/lib/angular/angular.js',
-      'app/lib/angular/angular-*.js',
-      'app/js/**/*.js',
-      'test/unit/**/*.js'
-    ],
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-    exclude : [
-      'app/lib/angular/angular-loader.js',
-      'app/lib/angular/*.min.js',
-      'app/lib/angular/angular-scenario.js'
-    ],
 
-    autoWatch : true,
-
+    // frameworks to use
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
 
-    plugins : [
-            'karma-junit-reporter',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-script-launcher',
-            'karma-jasmine'
-            ],
+    // list of files / patterns to load in the browser
+    files: [
+      '../app/js/vendor/angular/angular.js',
+      '../app/js/vendor/angular/angular-resource.js',
+      '../app/js/vendor/angular/angular-cookies.js',
+      '../app/js/vendor/angular/angular-route.js',
+      '../app/js/vendor/angular/angular-mocks.js',
+      '../app/js/vendor/sjcl/main.js',
+      '../app/js/bootstrap/app.js',
+      '../app/js/service/*.js',
+      '../test/jasmine/**/*.js'
+    ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
   });
 };
