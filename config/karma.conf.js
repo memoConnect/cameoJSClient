0d6448a42222808e4d3e304c673db89bef7b39b8
Michael Merz <michael.merz@indarium.de>
2014-01-30 22:57:59 +0100

    cleaned up, added package.json with needed dependencies, added grunt for fun

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,75 +1,76 @@
 // Karma configuration
 // Generated on Thu Jan 30 2014 20:39:59 GMT+0100 (CET)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
-
-
-    // frameworks to use
-    frameworks: ['jasmine'],
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      '../app/js/vendor/angular/angular.js',
-      '../app/js/vendor/angular/angular-resource.js',
-      '../app/js/vendor/angular/angular-cookies.js',
-      '../app/js/vendor/angular/angular-route.js',
-      '../app/js/vendor/angular/angular-mocks.js',
-      '../app/js/vendor/sjcl/main.js',
-      '../app/js/bootstrap/app.js',
-      '../app/js/service/*.js',
-      '../test/jasmine/**/*.js'
-    ],
+        // frameworks to use
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            '../app/js/vendor/angular/angular.js',
+            '../app/js/vendor/angular/angular-resource.js',
+            '../app/js/vendor/angular/angular-cookies.js',
+            '../app/js/vendor/angular/angular-route.js',
+            '../test/lib/angular/angular-mocks.js',
+            '../app/js/vendor/sjcl/main.js',
+            '../app/js/bootstrap/app.js',
+            '../app/js/service/*.js',
+            '../test/**/*Spec.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        //browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
