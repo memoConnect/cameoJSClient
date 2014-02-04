5934fe39e7f4130d1dc1445580c9181f3c6b84f8
Empujador <b.schaedel@xuffix.com>
2014-02-04 12:29:40 +0100

    update registry

--- app/js/controller/registry.js
+++ app/js/controller/registry.js
@@ -1,11 +1,16 @@
 'use strict';
-app.controller('RegistryCtrl', ['$scope', 'Auth',
-    function($scope, Auth){
+app.controller('RegistryCtrl', ['$scope','$location','Auth','cm',
+    function($scope,$location, Auth,cm){
         $scope.formData = {loginName:'',password:'',email:'',phoneNumber:'',name:''};
 
         $scope.regUser = function(){
             /**
-             * @ ToDo validate formData?
+             * @ ToDo validate formData
+             * if pw !empty && != none
+             * if cameoName != '' && not exists && min length
+             * if email then form ok?
+             * if phone then form ok?
+             *
              */
             var data = {
                 loginName: $scope.formData.cameoName,
@@ -16,12 +21,17 @@ app.controller('RegistryCtrl', ['$scope', 'Auth',
             };
 
             Auth.createUser(data).
-            success(function(res){
-                console.log(res);
-                //$location.path("/login");
+            success(function(r){
+                cm.log.debug('createUser success')
+                if(r.res == "OK"){
+                    $location.path("/login");
+                } else {
+                    // Notifiation to User
+                }
             }).
-            error(function(){
-                console.log('createUser Error');
+            error(function(r){
+                cm.log.debug('createUser Error')
+                // Notifiation to User res,error
             });
         };
 
