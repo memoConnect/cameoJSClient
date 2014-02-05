ce260ec9337ff5e81d7b60d3c7ea9d015cf71e6a
Empujador <b.schaedel@xuffix.com>
2014-02-05 12:02:30 +0100

    begin validation

--- app/js/controller/registry.js
+++ app/js/controller/registry.js
@@ -4,6 +4,14 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
         $scope.formData = {loginName:'',password:'',email:'',phoneNumber:'',name:''};
 
         $scope.regUser = function(){
+            var data = {
+                loginName: $scope.formData.cameoName,
+                password: $scope.formData.password,
+                email: "",
+                phoneNumber: $scope.formData.phone,
+                name: $scope.formData.name
+            };
+
             /**
              * @ ToDo validate formData
              * if pw !empty && != none
@@ -12,13 +20,19 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
              * if phone then form ok?
              *
              */
-            var data = {
-                loginName: $scope.formData.cameoName,
-                password: $scope.formData.password,
-                email: $scope.formData.email,
-                phoneNumber: $scope.formData.phone,
-                name: $scope.formData.name
-            };
+
+            // validate cameoName / loginName
+
+
+            //validate email
+            if($scope.formData.email != "" && validateEmail($scope.formData.email)){
+                data.email = $scope.formData.email;
+            }
+
+
+            console.log(data)
+            cm.log.debug("ende");
+            return false;
 
             AuthService.createUser(data).
             success(function(r){
@@ -34,6 +48,19 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
                 // Notifiation to User res,error
             });
         };
+
+        function validateEmail(mail){
+            if(angular.isUndefined(mail)){
+                return false;
+            }
+
+            var regex = '^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+.[a-zA-Z][a-zA-Z]+$';
+            if(regex.test(mail)){
+                return true;
+            }
+
+            return false;
+        }
 
     }
 ]);
