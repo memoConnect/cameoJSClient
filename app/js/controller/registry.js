53847f72c6f48ff6e7d4b53132597de259d565d6
Empujador <b.schaedel@xuffix.com>
2014-02-06 13:13:00 +0100

    add cmValidateEmail Directive

--- app/js/controller/registry.js
+++ app/js/controller/registry.js
@@ -31,6 +31,10 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
 
             //validate email
             if($scope.registryForm.email.$valid == false){
+                // http://stackoverflow.com/a/46181/11236
+
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                console.log(re.test(email));
                 cm.notify.warn("E-Mail has wrong format!");
                 return false;
             }
@@ -70,3 +74,27 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
 
     }
 ]);
+
+app.directive('cmValidateEmail',function(){
+    //http://stackoverflow.com/questions/16863389/angular-js-email-validation-with-unicode-characters
+    return {
+        require: 'ngModel',
+        link: function(scope,element,attrs,model){
+            element.on('blur', function(evt){
+                scope.$apply(function(){
+                    var val = element.val();
+                    var check = true;
+                    if(val != ""){
+                        // http://stackoverflow.com/a/46181/11236
+                        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        check = re.test(val);
+                    }
+                    console.log(check);
+                    if(check !== true){
+                        model.$setValidity('email', false);
+                    }
+                });
+            });
+        }
+    }
+});
