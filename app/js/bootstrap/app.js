2fd037bcb7524f42d96e5f67cdb71af1fe0631d6
Reimerei <git@bjrm.de>
2014-02-07 18:25:49 +0100

    fix compile error

--- app/js/bootstrap/app.js
+++ app/js/bootstrap/app.js
@@ -1,17 +1,14 @@
 'use strict';
 var cameo = {
     //restApi:              "http://"+location.host+"/api"
-    restApi:                "https://dev.cameo.io/api/v1"
-   ,token:                  null
-   ,supported_languages:    ['de_DE', 'en_US']
-   ,path_to_languages:      'languages'
+    restApi: "https://dev.cameo.io/api/v1", token: null, supported_languages: ['de_DE', 'en_US'], path_to_languages: 'languages'
 };
 
 
 var app = angular.module('cameoClient', [
 
     'ngRoute',
-    'ngCookies',    
+    'ngCookies',
     'cmApiAuth',
     'cmCrypt',
     'cmLanguage',
@@ -21,88 +18,89 @@ var app = angular.module('cameoClient', [
 
 ]);
 
-app.service('cm',[
+app.service('cm', [
 
     'cmApi',
     'cmAuth',
     'cmCrypt',
     'cmLogger',
     'cmNotify',
-    'cmTranslate',    
-    
+    'cmTranslate',
 
-    function(cmApi, cmAuth, cmCrypt, cmLogger, cmNotify, cmTranslate){
+
+    function (cmApi, cmAuth, cmCrypt, cmLogger, cmNotify, cmTranslate) {
         return {
-            log:        cmLogger,
-            notify:     cmNotify,
-            translate:  cmTranslate,
-            crypt:      cmCrypt,
-            api:        cmApi,
-            auth:       cmAuth
+            log: cmLogger,
+            notify: cmNotify,
+            translate: cmTranslate,
+            crypt: cmCrypt,
+            api: cmApi,
+            auth: cmAuth
         }
     }
 ])
 
 app.config(['$routeProvider', '$locationProvider',
-function($routeProvider, $locationProvider){
-    //$locationProvider.html5Mode(true);
-    $routeProvider.
-    when('/login', {
-        templateUrl: 'tpl/form/login.html',
-        controller: 'LoginCtrl'
-    }).
-    when('/start', {
-        templateUrl: 'tpl/start.html',
-        controller: 'StartCtrl'
-    }).
-    when('/talks', {
-        templateUrl: 'tpl/list/talks.html',
-        controller: 'TalksCtrl'
-    }).
-    when('/mediawall', {
-        templateUrl: 'tpl/list/mediawall.html',
-        controller: 'MediaWallCtrl'
-    }).
-    when('/conversation/:conversationId', {
-        templateUrl: 'tpl/conversation.html',
-        controller: 'ConversationCtrl'
-    }).
-    when('/registry', {
-        templateUrl: 'tpl/form/registry.html',
-        controller: 'RegistryCtrl'
-    }).
-    when('/filter', {
-        templateUrl: 'tpl/form/filter.html',
-        controller: 'FilterCtrl'
-    when('/profile', {
-        templateUrl: 'tpl/form/profile.html',
-        controller: 'ProfileCtrl'
-    }).
-    when('/verification/:secret', {
-        templateUrl: 'tpl/verification.html',
-    }).
-    otherwise({
-        redirectTo: '/login'
-    });
-}]);
+    function ($routeProvider, $locationProvider) {
+        //$locationProvider.html5Mode(true);
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'tpl/form/login.html',
+                controller: 'LoginCtrl'
+            }).
+            when('/start', {
+                templateUrl: 'tpl/start.html',
+                controller: 'StartCtrl'
+            }).
+            when('/talks', {
+                templateUrl: 'tpl/list/talks.html',
+                controller: 'TalksCtrl'
+            }).
+            when('/mediawall', {
+                templateUrl: 'tpl/list/mediawall.html',
+                controller: 'MediaWallCtrl'
+            }).
+            when('/conversation/:conversationId', {
+                templateUrl: 'tpl/conversation.html',
+                controller: 'ConversationCtrl'
+            }).
+            when('/registry', {
+                templateUrl: 'tpl/form/registry.html',
+                controller: 'RegistryCtrl'
+            }).
+            when('/filter', {
+                templateUrl: 'tpl/form/filter.html',
+                controller: 'FilterCtrl'
+            }).
+            when('/profile', {
+                templateUrl: 'tpl/form/profile.html',
+                controller: 'ProfileCtrl'
+            }).
+            when('/verification/:secret', {
+                templateUrl: 'tpl/verification.html'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }]);
 
 app.run(['$rootScope', '$location', '$cookieStore',
-function($rootScope, $location, $cookieStore){
-    $rootScope.$on( "$routeChangeStart", function(){
+    function ($rootScope, $location, $cookieStore) {
+        $rootScope.$on("$routeChangeStart", function () {
 //        var path_exceptions = ['/login', '/registry'];
-        var path = $location.$$path;
+            var path = $location.$$path;
 
-       if(angular.isUndefined($cookieStore.get("token"))){
-            if(path != "/login" && path != "/registry"){
-                $location.path("/login");
+            if (angular.isUndefined($cookieStore.get("token"))) {
+                if (path != "/login" && path != "/registry") {
+                    $location.path("/login");
+                }
+            } else if ($location.$$path == "/login") {
+                $location.path("/start");
             }
-        } else if($location.$$path == "/login"){
-            $location.path("/start");
-        }
 
-        if(angular.isDefined($cookieStore.get("token"))){
-            cameo.token = $cookieStore.get("token");
-        }
-        
-    });
-}]);
+            if (angular.isDefined($cookieStore.get("token"))) {
+                cameo.token = $cookieStore.get("token");
+            }
+
+        });
+    }]);
