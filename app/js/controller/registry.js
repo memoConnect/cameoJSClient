72366c8f9f44cfaad84bc9ece71ea5a3b8da7ba0
Empujador <b.schaedel@xuffix.com>
2014-02-07 09:22:53 +0100

    update code style

--- app/js/controller/registry.js
+++ app/js/controller/registry.js
@@ -1,13 +1,13 @@
 'use strict';
-app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
-    function($scope,$location, AuthService, cm){
-        $scope.formData = {loginName:'',password:'',email:'',phoneNumber:'',name:''};
+app.controller('RegistryCtrl', ['$scope', '$location', 'AuthService', 'cm',
+    function ($scope, $location, AuthService, cm) {
+        $scope.formData = {loginName: '', password: '', email: '', phoneNumber: '', name: ''};
 
         /**
          * @ ToDo validate formData
          * check LoginName -> reservation Secrect
          */
-        $scope.regUser = function(){
+        $scope.regUser = function () {
             var data = {
                 loginName: null,
                 password: null,
@@ -18,7 +18,7 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
             };
 
             // check cameoName == loginName
-            if($scope.registryForm.cameoName.$valid == false){
+            if ($scope.registryForm.cameoName.$valid == false) {
                 cm.notify.warn("Username is required!");
                 return false;
             } else {
@@ -26,7 +26,7 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
             }
 
             // check password
-            if($scope.formData.password == '' || $scope.formData.password == 'none'){
+            if ($scope.formData.password == '' || $scope.formData.password == 'none') {
                 cm.notify.warn("Password is required!");
                 return false;
             } else {
@@ -34,23 +34,23 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
             }
 
             // check email
-            if($scope.registryForm.email.$valid == false){
+            if ($scope.registryForm.email.$valid == false) {
                 // http://stackoverflow.com/a/46181/11236
                 cm.notify.warn("E-Mail has wrong format!");
                 return false;
             } else {
-                if($scope.formData.email != ''){
+                if ($scope.formData.email != '') {
                     data.email = $scope.formData.email;
                 }
             }
 
             // check name
-            if($scope.formData.name != ''){
+            if ($scope.formData.name != '') {
                 data.name = $scope.formData.name;
             }
 
             // check agb
-            if($scope.registryForm.agb.$valid == false){
+            if ($scope.registryForm.agb.$valid == false) {
                 cm.notify.warn("Confirm AGB!");
                 return false;
             }
@@ -60,18 +60,18 @@ app.controller('RegistryCtrl', ['$scope','$location','AuthService','cm',
             return false;
 
             AuthService.createUser(data).
-            success(function(r){
-                cm.log.debug('createUser success')
-                if(r.res == "OK"){
-                    $location.path("/login");
-                } else {
-                    // Notifiation to User
-                }
-            }).
-            error(function(r){
-                cm.log.debug('createUser Error')
-                // Notifiation to User res,error
-            });
+                success(function (r) {
+                    cm.log.debug('createUser success')
+                    if (r.res == "OK") {
+                        $location.path("/login");
+                    } else {
+                        // Notifiation to User
+                    }
+                }).
+                error(function (r) {
+                    cm.log.debug('createUser Error')
+                    // Notifiation to User res,error
+                });
         };
     }
 ]);
