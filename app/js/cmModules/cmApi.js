c6f05b9bcd3b73efe34b73e58821cead54256b78
Andreas Pittrich <andreas.pittrich@web.de>
2014-02-13 12:49:44 +0100

    Lots of comments added to cmApi, Authetication token no longer in query, Authetication token remains in header.

--- app/js/cmModules/cmApi.js
+++ app/js/cmModules/cmApi.js
@@ -33,37 +33,155 @@ define([
 			'$q',
 
 			function(cmLogger, $http, $injector, $q){
+				/***
+				All api calls require a config object:
 
-				//cmApi can only perfom calls that need authentication if cmAuth is present
+				ie.: api.get(config)
+
+				config works almost like in $http(config)
+
+				most important keys are:					
+					url:	api path to call i.e. '/account/check', 
+							will give an error message if passed something different from a path (like 'http://dev.cameo.io/...')
+							in that case your call will most likely fail brutally
+
+					data:	data to send, any plain object
+
+					exp_ko: key you expect in response body if your request was granted(see below)
+					exp_ok: key you expect in response body if your request was denied (see below)
+
+
+				Authentication and error handling is dealt with automatically.
+
+
+				example:
+
+                cmApi.get({
+                    url:     '/pony',
+                    exp_ok:  'pony',
+                })
+
+                
+                ---> response:  {
+                                    "res" : 'OK',
+                                    "data": {
+												"pony" : "my_new_pony"
+											}
+								}
+
+				.then(
+					function(pony){         <--- gets called because response.res == 'OK', pony will equal 'my_pony'
+						yay(pony)
+					},
+
+					function(alternative, res){	
+						alternative												 
+						? meh(alternative)
+						: error(alternative) //yet error should have already been handled alesewhere
+					}
+				)
+
+
+				---> response:  {
+                                    "res" : 'OK',
+                                    "data": {
+												"dog" : "my_new_dog"
+											}
+								}
+
+				.then(
+					function(pony){      
+						yay(pony)
+					},
+					function(alternative,res){	<--- gets called because response is invalid, "pony" was expected, yet "dog" was delivered
+												     alternative will be undefined
+												     res however holds all the response 
+						alternative												 
+						? meh(alternative)
+						: error(alternative) //yet error should have been handled already elesewhere
+					}
+				)
+
+
+
+
+				---> response:	{
+									"res" : 'KO',
+									"data": {
+												"alternative" : "kitty"
+											}
+								}
+
+				.then(
+					function(pony){
+						yay(pony)
+					},
+					function(data, res){ <--- gets called because response.res == 'KO', data will be {'alternative': 'kitty'},
+					                          because there was no specific key expected for KO.
+					                          res however holds all the response
+						alternative												 
+						? meh(alternative)
+						: error(alternative) //yet error should have been handled already elesewhere
+					}
+				)
+
+
+
+
+				---> response:	{
+									"res" : 'XXX',
+									"data": {
+												"kitty" : "grumpy cat"
+											}
+								}
+
+				.then(
+					function(pony){
+						yay(pony)
+					},
+                    function(alternative,res){ <--- gets called because response is invalid for neither response.res == 'OK' nor response.res == 'KO', 
+                                                    alternative will be undefined
+                                                    res however holds all the response
+						alternative												 
+						? meh(alternative)
+						: error(alternative) //yet error should have been handled already elesewhere
+					}
+				)
+
+
+
+				*/
+
 				var api 		=	function(method, config){
 										var deferred	=	$q.defer(),
+
+											//get authentification token from cmAuth
 											token 		= 	$injector.has('cmAuth')
 															?	$injector.get('cmAuth').getToken()
-															:	undefined,
+															:	undefined											
 
-											token_param = 	token 
-															?	(config.url.match(/\?/) ? '&token=' : '?token=') +	//add or extend paramters
-																token												//add auth token							
-															:	''
-
+										//Api calls are restricted to the preconfigured api base path and should start with a '/'																
 										if(!config.url.match(/^\//g)) cmLogger.error('Api calls are restricted to '+rest_api+' . You tried:', config)
 
 										//extend or overwrite config
-										config			=	config || {}
-										config.method	= 	method
+										config			=	config || {}	// make sure config is defined
+										config.method	= 	method			// overwrite method
 										config.url		= 	rest_api +		// base url API
-															config.url +	// path to specific method
-															token_param
-										config.headers	=	angular.extend({}, {'Authorization': token}, config.headers || {})
+															config.url 		// path to specific method															
+										config.headers	=	angular.extend({}, {'Authorization': token}, config.headers || {})	//add authorization token to the header
 
 
-										function responseValid(response, exp_ok_key, exp_ko_key){
-											var valid =    response
-														&& response.res
-														&& (response.res == "OK" && exp_ok_key ? response.data[exp_ok_key] !== undefined : true)
-														&& (response.res == "KO" && exp_ko_key ? response.data[exp_ko_key] !== undefined : true) 
+										//check if the response matches the api conventions
+										function responseValid(response, exp_ok, exp_ko){
+											var valid =    response			
+															//response must have a res key that equals 'OK' or 'KO':
+														&& (response.res == 'OK' || response.res == 'OK')
+															//if your request was granted and something was expected in return, it must be present:
+														&& (response.res == "OK" && exp_ok ? response.data[exp_ok] !== undefined : true)
+															//if your request was denied and something was expected in return, it must be present:
+														&& (response.res == "KO" && exp_ko ? response.data[exp_ko] !== undefined : true) 
 
-											if(!valid) cmLogger.error('Api response invalid; expected '+ (exp_ok_key||'') +', '+(exp_ko_key||''), response)
+											if(!valid) cmLogger.error('Api response invalid; '+(exp_ok||exp_ko ? 'expected: ':'') + (exp_ok||'') +', '+(exp_ko||''), response)
 
 											return(valid)
 										}
@@ -71,21 +189,33 @@ define([
 										$http(config).then(											
 
 											function(response){	
-												//$http calls success and error function with an object containing config AND data, we only need the data:
+												//$http call was successfull:
+												//reponse includes config and data, we only need the data:
 												var response = response.data
 
 												responseValid(response, config.exp_ok, config.exp_ko)
 												?	//response valid, check if OK:
-													response.res =='OK'
+													//if a certain key was expected, resolve promise resp. reject the promise with the according values
+													//if nothing was expected, just resolve or reject with value of 'data' in the response body if present or all the data
+													//reponse should now look similar to this:
+													/*
+														"res":	"OK",
+														"data": {
+																	"some_key": 			"some_value",
+																	"some expected_key": 	"some_other value"
+																}
+
+													*/
+													response.res =='OK'													
 													? deferred.resolve(	config.exp_ok ? response.data[config.exp_ok] : response.data || response)
 													: deferred.reject(	config.exp_ko ? response.data[config.exp_ko] : response.data || response)
-												:	//response invalid, call through:
-													deferred.reject(response)
+												:	//response invalid, call through:													
+													deferred.reject(undefined, response)
 											},
 
 											function(response){
 												//error messages should come trough backend
-												deferred.reject(response)
+												deferred.reject(undefined, response)
 											}
 										)
 															
