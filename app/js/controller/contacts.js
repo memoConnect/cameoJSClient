98bcc566fbd8afa3a038066070b60c4ff50761ed
Empujador <b.schaedel@xuffix.com>
2014-02-14 11:02:31 +0100

    begin with search directive

--- app/js/controller/contacts.js
+++ app/js/controller/contacts.js
@@ -11,4 +11,25 @@ define([
         function($scope,$location,cmNotify,cmLogger){
 
         }]);
+
+    app.register.directive('cmSearchCameoId',
+        function(cmLogger){
+            return {
+                restrict: 'E',
+                scope: {},
+                templateUrl: 'tpl/directives/cm-search-cameo-id.html',
+                controller: function($scope, $element, $attrs){
+                    var mockResults = ['derMicha','dasEmpu','dutscher','reimerei','rhotp'];
+                    $scope.results = [];
+
+                    $scope.showResults = false;
+                    $scope.showNoResults = false;
+
+                    $scope.search = function(){
+                       $scope.showResults = true;
+                       $scope.results = mockResults;
+                    }
+                }
+            }
+        });
 });
