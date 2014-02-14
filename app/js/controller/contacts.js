f8b2e1266f57654311056ef742e531acd528b39b
dutscher <mocloVn0twar>
2014-02-14 14:46:53 +0100

    contacts directiven

--- app/js/controller/contacts.js
+++ app/js/controller/contacts.js
@@ -3,6 +3,8 @@ define([
 ], function(app){
    'use strict';
 
+    var mockResults = ['derMicha','dasEmpu','dutscher','reimerei','rhotp'];
+
     app.register.controller('ContactsCtrl',[
         '$scope',
         '$location',
@@ -17,9 +19,8 @@ define([
             return {
                 restrict: 'E',
                 scope: {},
-                templateUrl: 'tpl/directives/cm-search-cameo-id.html',
+                templateUrl: 'tpl/modules/contacts/cm-search-cameo-id.html',
                 controller: function($scope, $element, $attrs){
-                    var mockResults = ['derMicha','dasEmpu','dutscher','reimerei','rhotp'];
                     $scope.results = [];
 
                     $scope.showResults = false;
@@ -33,6 +34,28 @@ define([
 
                     $scope.sendFriendshipRequest = function(cameoId){
                         cmLogger.debug('sendFriendshipRequest to '+cameoId);
+                        // TODO: cmApi stuff
+                    };
+                }
+            }
+        });
+
+    app.register.directive('cmContactsList',
+        function(cmApi, cmLogger){
+            return {
+                restrict: 'E',
+                scope: {},
+                templateUrl: 'tpl/modules/contacts/cm-contacts-list.html',
+                controller: function($scope, $element, $attrs){
+                    $scope.contacts = mockResults;
+
+                    $scope.editContact = function(){
+                        cmLogger.debug('editContact '+cameoId);
+                        // TODO: cmApi stuff
+                    };
+
+                    $scope.deleteContact = function(cameoId){
+                        cmLogger.debug('deleteContact '+cameoId);
                         // TODO: cmApi stuff
                     };
                 }
