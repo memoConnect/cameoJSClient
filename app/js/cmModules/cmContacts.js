dcfe0a2333230a6ac8043ec9a2b94500f5a8f9bf
Empujador <b.schaedel@xuffix.com>
2014-02-18 21:46:34 +0100

    add util

--- app/js/cmModules/cmContacts.js
+++ app/js/cmModules/cmContacts.js
@@ -1,5 +1,5 @@
 define([
-    'angular',
+    'app',
     'util'
 ], function () {
    'use strict';
--- app/js/controller/contacts.js
+++ app/js/controller/contacts.js
@@ -42,8 +42,6 @@ define([
                             return false;
                         }
 
-                        cmLogger.debug($scope.string);
-
                         cmContacts.searchCameoId($scope.string).
                         then(
                             function(data){
