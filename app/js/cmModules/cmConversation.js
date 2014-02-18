6d9f5ddf4a8b563c4bba1b698d4a4216a0111b5c
Andreas Pittrich <andreas.pittrich@web.de>
2014-02-18 15:10:14 +0100

    Message Directive funktionionstüchtig gemacht

--- app/js/cmModules/cmConversation.js
+++ app/js/cmModules/cmConversation.js
@@ -28,6 +28,7 @@ define([
 			function($q, cmApi){
 				return {
 					getConversation: function(id, offset, limit){
+						/*
 						return 	cmApi.get({
 									url: 	'/conversation/'+id,
 									data:	{
@@ -36,6 +37,104 @@ define([
 											},
 									exp_ok:	'messages'
 								})
+						*/
+
+						//Mock:
+						var deferred = $q.defer()
+
+						deferred.resolve({
+						    "id": "WvbpPu2X8btdAOpA4IRF",
+						    "messages": [
+						      {
+						        "id": "vwUmVfjkn5lXXwXGvxzS",
+						        "messageBody": "text",
+						        "fromIdentity": "ZVtXkxMmPj4WtLYea8cix",
+						        "messageStatus": [
+						          {
+						            "identityId": "s29FpU4TFhxXOdRGfzLk",
+						            "status": "queued",
+						            "message": "default"
+						          },
+						          {
+						            "identityId": "V8SXRiE4o1lfN2WmGf0S",
+						            "status": "queued",
+						            "message": "sms"
+						          },
+						          {
+						            "identityId": "j07EKjJiGOXdfx2wqs0N",
+						            "status": "queued",
+						            "message": "default"
+						          }
+						        ],
+						        "created": "22.01.2014 16:34:55"
+						      },
+						      {
+						        "id": "6ftwTSBMiQfmlOphLND3",
+						        "messageBody": "text2",
+						        "fromIdentity": "ZVtXkxMmPj4WtLYea8ci",
+						        "messageStatus": [
+						          {
+						            "identityId": "s29FpU4TFhxXOdRGfzLk",
+						            "status": "queued",
+						            "message": "default"
+						          },
+						          {
+						            "identityId": "V8SXRiE4o1lfN2WmGf0S",
+						            "status": "queued",
+						            "message": "sms"
+						          },
+						          {
+						            "identityId": "j07EKjJiGOXdfx2wqs0N",
+						            "status": "queued",
+						            "message": "default"
+						          }
+						        ],
+						        "created": "22.01.2014 16:34:55"
+						      },
+						      {
+						        "id": "O5uz1fBnuWFRwnqOCGuN",
+						        "messageBody": "text3",
+						        "fromIdentity": "ZVtXkxMmPj4WtLYea8cix",
+						        "messageStatus": [
+						          {
+						            "identityId": "s29FpU4TFhxXOdRGfzLk",
+						            "status": "queued",
+						            "message": "default"
+						          },
+						          {
+						            "identityId": "V8SXRiE4o1lfN2WmGf0S",
+						            "status": "queued",
+						            "message": "sms"
+						          },
+						          {
+						            "identityId": "j07EKjJiGOXdfx2wqs0N",
+						            "status": "queued",
+						            "message": "default"
+						          }
+						        ],
+						        "created": "22.01.2014 16:34:55"
+						      }
+						    ],
+						    "numberOfMessages": 3,
+						    "created": "22.01.2014 16:34:55",
+						    "lastUpdated": "22.01.2014 16:34:55",
+						    "recipients": [
+						      {
+						        "id": "s29FpU4TFhxXOdRGfzLk",
+						        "displayName": "NoName"
+						      },
+						      {
+						        "id": "V8SXRiE4o1lfN2WmGf0S",
+						        "displayName": "Ulrich"
+						      },
+						      {
+						        "id": "j07EKjJiGOXdfx2wqs0N",
+						        "displayName": "Aaron"
+						      }
+						    ]
+						})
+
+						return deferred.promise
 					}
 
 				}
@@ -71,17 +170,71 @@ define([
 		}
 	])
 
+
 	cmConversation.directive('cmMessage',[
 
 		function(){
 			return {
 
 				restrict: 		'AE',
+				scope:			true,
 				templateUrl:	'tpl/modules/conversation/cm-message.html',
 
 				controller:		function($scope, $element, $attrs){
-									$scope.message		= $attrs.data									 
+
+									function getMyOwnIdentity(){
+										//MOCK
+										return "ZVtXkxMmPj4WtLYea8ci"
+									}
+
+									$scope.message			 = $scope.$eval($attrs.cmMessage || $attrs.cmData)									
+									$scope.is_my_own_message = ($scope.message.fromIdentity == getMyOwnIdentity())
 								}
+			}
+		}
+	])
+
+
+	cmConversation.directive('cmAvatar',[
+
+		function(){
+			return {
+
+				restrict: 		'AE',
+				template:		'<i class="fa fa-user"></i>', //MOCK
+				link:			function(scope, element, attrs){
+									 //mocked, get avatar pic an set background of element
+									 element.css({
+									 	"display":			"inline-block",
+									 	"cssFloat":			"left",
+									 	"font-size":		"3em",
+									 	"vertical-align":	"top",
+									 	"padding-right":	"0.3em"
+									 })
+								}
+
+				
+			}
+		}
+	])
+
+	cmConversation.directive('cmAttachments',[
+
+		function(){
+			return {
+
+				restrict: 		'AE',
+				template:		'<i class="fa fa-paper-clip"></i>', //MOCK
+				link:			function(scope, element, attrs){
+									 //mocked
+									 element.css({
+									 	"display":			"inline-block",									 	
+									 	"font-size":		"2em",
+									 	"vertical-align":	"middle"
+									 })
+								}
+
+				
 			}
 		}
 	])
