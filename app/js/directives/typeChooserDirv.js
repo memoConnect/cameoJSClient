eca9248b8d70f4571b93c81406b9379b7c7b9e04
dutscher <mocloVn0twar>
2014-02-21 08:55:08 +0100

    changes for new directives

--- app/js/directives/typeChooserDirv.js
+++ app/js/directives/typeChooserDirv.js
@@ -5,39 +5,64 @@ define([
 
     app.register.directive('cmTypeChooser',
         function(cmLogger){
-            return {
-                restrict: 'A',
-                scope: {
-                    chooseValue: '='
-                },
-                templateUrl: 'tpl/directives/cm-type-chooser.html',
-                link: function($scope, $element, $attrs){
+            // defined types
+            var types = {
+                types: [
+                    {i18n:'DIRV.TYPE_CHOOSER.PRIVATE',value:'private'},
+                    {i18n:'DIRV.TYPE_CHOOSER.BUSINESS',value:'business'},
+                    {i18n:'DIRV.TYPE_CHOOSER.OTHER',value:'other','default':true}
+                ],
+                provider: [
+                    {i18n:'DIRV.TYPE_CHOOSER.LANDLINE',value:'landline'},
+                    {i18n:'DIRV.TYPE_CHOOSER.IP',value:'ip'},
+                    {i18n:'DIRV.TYPE_CHOOSER.FAX',value:'fax'},
+                    {i18n:'DIRV.TYPE_CHOOSER.MOBILE',value:'mobile','default':true}
+                ]
+            };
 
+            return {
+                scope: true,
+                templateUrl: 'js/directives/typeChooser.html',
+                controller: function($scope, $element, $attrs){
+                    // handle special type of choose default
+                    if('chooseType' in $attrs && $attrs.chooseType in types){
+                        $scope.buttons = types[$attrs.chooseType];
+                    } else {
+                        $scope.buttons = types.types;
+                    }
+
+                    /**
+                     * search in buttons array for value or default
+                     * @string value
+                     * @boolean searchDefault
+                     * @returns {string}
+                     */
                     function find(value, searchDefault){
                         var btn = "";
-                        angular.forEach($scope.buttons,function(button){
+                        angular.forEach($scope.buttons, function(button){
                             if(searchDefault != undefined && 'default' in button){
                                 btn = button.value;
                             }
                             if(value != undefined && button.value == value){
                                 btn = button.value;
                             }
-                        })
-                        return btn;
+                        });
+                        return btn
                     }
 
-                    $scope.buttons = [
-                        {i18n:'TYPE_CHOOSER.PRIVATE',value:'private'},
-                        {i18n:'TYPE_CHOOSER.BUSINESS',value:'business'},
-                        {i18n:'TYPE_CHOOSER.OTHER',value:'other','default':true}
-                    ];
-
+                    /**
+                     * set button to active and set parent scope variable
+                     * @string value
+                     */
                     $scope.setActive = function(value){
-                        $scope.chooseValue = find(value);
+                        $scope.active = find(value);
+                        $scope.data[$attrs.chooseToData] = $scope.active;
                     };
 
-                    $scope.setActive(find(null, true));
+                    // set default button
+                    $scope.setActive(find(null, true))
                 }
             }
-        });
-})
+        })
+    return app;
+});
