63363c9e3e6e118c0b873c584591cc64fe5ca145
Empujador <b.schaedel@xuffix.com>
2014-02-21 16:09:54 +0100

    add checkKeyExists to Util

--- app/js/controller/purl.js
+++ app/js/controller/purl.js
@@ -1,6 +1,9 @@
 define([
     'app',
-    'mUser'
+    'mUser',
+    'cmApi',
+    'cmLogger',
+    'util'
 ], function(app){
     'use strict';
 
@@ -8,10 +11,28 @@ define([
         '$scope',
         '$routeParams',
         'ModelUser',
-        function($scope, $routeParams,ModelUser){
-            console.log($routeParams)
+        'cmApi',
+        'cmLogger',
+        'Util',
+        function($scope, $routeParams, ModelUser, cmApi, cmLogger, Util){
+            $scope.data = null;
 
 
+            if(Util.checkKeyExists($routeParams,'idPurl') && Util.validateString($routeParams.idPurl)){
+                console.log($routeParams.idPurl + " valid");
+            } else {
+                console.log('moep')
+            }
+
+
+            cmApi.get({url:'/purl/'+$routeParams.idPurl}).then(
+                function(data){
+                    $scope.data = data;
+                },
+                function(){
+                    cmLogger.error('cant get PURL Message');
+                }
+            );
         }
     ]);
 });
