be09ebdc3dd3424ac248d8a4c1e294b58965c4c4
dutscher <dutscher@cameo.io>
2014-03-03 18:27:37 +0100

    pusching

--- test/lib/requirejs/ngload.js
+++ test/lib/requirejs/ngload.js
@@ -5,14 +5,14 @@
 define({
     load: function (name, req, onload, config) {
         //console.log("ngamd loaded: ", req.toUrl(name));
-        req(['app','angularAMD', name], function (app, angularAMD, value) {
+        req(['app', 'angularAMD', name], function (app, angularAMD, value) {
 
-            try {
-                //console.log('bootstrapped manual');
-                angularAMD.bootstrap(app);
-            } catch (e){
-                //console.log('bootstrapped is done '+e);
-            }
+//            try {
+//                //console.log('bootstrapped manual');
+//                angularAMD.bootstrap(app);
+//            } catch (e){
+//                //console.log('bootstrapped is done '+e);
+//            }
 
             //console.log("Processing queues.");
             angularAMD.processQueue();
