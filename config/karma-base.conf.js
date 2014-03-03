0add38b2719adbd3c1991d0896245d5b5ae1f998
Empujador <b.schaedel@xuffix.com>
2014-03-03 09:38:35 +0100

    update spec tests

--- config/karma-base.conf.js
+++ config/karma-base.conf.js
@@ -11,9 +11,8 @@ module.exports = function(config) {
             // all frontend files
             {pattern: 'app/**/*.js', included: false},
             // tests
-//            {pattern: 'test/unit/**/crypt.spec.js', included: false},
-            {pattern: 'test/unit/**/*.spec_fix.js', included: false},
-//            {pattern: 'test/unit/**/*.amd-spec.js', included: false},
+            {pattern: 'test/unit/**/*.fix.spec.js', included: false},
+            {pattern: 'test/unit/**/*.fix.amd-spec.js', included: false},
 
 //            {pattern: 'test/unit/**/app.amd-spec.js', included: false},
 //            {pattern: 'test/unit/**/auth.amd-spec.js', included: false},
