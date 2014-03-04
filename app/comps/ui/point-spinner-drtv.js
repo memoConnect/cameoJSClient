63369dec194b5bdc00514f86fb31f71841a70804
rhotep <andreas.pittrich@posteo.de>
2014-03-04 14:12:19 +0100

    pckages 79 tests

---
+++ app/comps/ui/point-spinner-drtv.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function cmPointSpinner(){
+    return {
+        scope: true,
+        template: '<span ng-show="isIdle" style="width:20px;display:inline-block;text-align:left">{{points}}</span>',
+        controller: function($scope, $interval){
+            var interval;
+
+            $scope.isIdle = false;
+            $scope.points = "";
+
+            $scope.$on('cmPointSpinner:start',function(){
+                $scope.isIdle = true;
+                interval = $interval(function(){
+                    $scope.points += ".";
+
+                    if($scope.points.length == 4){
+                        $scope.points = "";
+                    }
+                },250);
+            });
+
+            $scope.$on('cmPointSpinner:cancel',function(){
+                $scope.isIdle = false;
+                $interval.cancel(interval);
+            });
+        }
+    }
+}
---
+++ app/comps/ui/ui-module.js
@@ -0,0 +1,10 @@
+var cmUi = angular.module('cmUi', [])
+
+cmUi.directive('cmNavTabs',[
+    cmNavTabs
+])
+
+
+cmUi.directive('cmPointSpinner',[
+    cmPointSpinner
+])
