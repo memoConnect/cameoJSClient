4cbd391ff8c13523dd2cef89b62830013caaf45f
Reimerei <git@bjrm.de>
2014-03-05 11:51:20 +0100

    updated phonegap script

--- phonegap.sh
+++ phonegap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-./setup.sh
+./setup.sh 
 
-echo -e "\e[33m[cameo - add device to adb]\033[0m"
+if [ ! -z "$1" ]; then
+	echo -e "\e[33m[cameo - connecting to adb device at $1:5555]\033[0m"
+	adb connect $1:5555
+fi
 
-adb connect 192.168.178.46:5555
-adb devices
+device=$(adb devices | head -2 | tail -1)
+
+if [ -z "$device" ]; then
+	echo -e "\e[33m[cameo - No device found]\033[0m"
+	exit 1
+else
+	echo -e "\e[33m[cameo - Deploying to device: $device]\033[0m"
+fi
 
 echo -e "\e[33m[cameo - create phonegap]\033[0m"
 ./node_modules/grunt-cli/bin/grunt phonegap
