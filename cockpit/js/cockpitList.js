ab431507ba7e520c51359af8c9e215c03df57c51
Reimerei <git@bjrm.de>
2014-03-13 11:09:29 +0100

    added background for index.html

--- cockpit/js/cockpitList.js
+++ cockpit/js/cockpitList.js
@@ -10,42 +10,65 @@ cockpitList.controller("cockpitListCtrl", [
     function ($scope, cmApi, cmLogger, $routeParams, $location) {
 
         $scope.name = $routeParams.elementName
+        $scope.filters = []
+        $scope.showAlert = false
         $scope.list = []
         $scope.titles = []
-
-        console.log($routeParams)
+        $scope.selectedFilter = "choose filter"
 
         var filterSettings = {
             offset: 0,
-            limit: 20
+            limit: 20,
+            filter: []
         }
 
         updateList()
 
         function updateList() {
+            filterSettings.offset = 0
             cmApi.post({
                 url: '/' + $routeParams.elementName,
                 data: filterSettings
             }).then(
                 function (data) {
+                    $scope.showAlert = false
                     if (data.elements.length > 0) {
                         $scope.titles = data.titles
-                        $scope.list = $scope.list.concat(data.elements)
+                        $scope.filters = data.availableFilters
+                        $scope.list = data.elements
                     }
+                },
+                function() {
+                    $scope.showAlert = true
                 }
             )
         }
 
         $scope.loadMore = function () {
             filterSettings.offset += filterSettings.limit
-            updateList()
+            cmApi.post({
+                url: '/' + $routeParams.elementName,
+                data: filterSettings
+            }).then(
+                function (data) {
+                    $scope.showAlert = false
+                    if (data.elements.length > 0) {
+                        $scope.titles = data.titles
+                        $scope.filters = data.availableFilters
+                        $scope.list = $scope.list.concat(data.elements)
+                    }
+                },
+                function() {
+                   $scope.showAlert = true
+                }
+            )
         }
 
         $scope.editElement = function (id) {
             $location.url('/' + $scope.name + '/' + id)
         }
 
-        $scope.createNew = function() {
+        $scope.createNew = function () {
             cmApi.post({
                 url: '/' + $routeParams.elementName + '/new'
             }).then(
@@ -57,7 +80,6 @@ cockpitList.controller("cockpitListCtrl", [
         }
 
         $scope.deleteElement = function (id) {
-
             cmApi.delete({
                 url: '/' + $routeParams.elementName + "/" + id
             }).then(
@@ -74,6 +96,25 @@ cockpitList.controller("cockpitListCtrl", [
             )
         }
 
+        $scope.setFilter = function (newFilter) {
+            $scope.selectedFilter = newFilter
+        }
+
+        $scope.sendFilter = function () {
+            $scope.list = []
+            filterSettings.filter = [{
+                name: $scope.selectedFilter,
+                term: $scope.filterTerm
+            }]
+            updateList()
+        }
+
+        $scope.foo = function(event) {
+            if(event.keyCode == 13) {
+                $scope.sendFilter()
+            }
+            console.log(event.keyCode)
+        }
 
     }
 ])
