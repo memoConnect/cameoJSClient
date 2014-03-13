7a432dfd8909baca7400c5d16d1f266b2e74bb46
Empujador <b.schaedel@xuffix.com>
2014-03-13 18:13:42 +0100

    toggle rhino and text

--- app/routes/login/login-ctrl.js
+++ app/routes/login/login-ctrl.js
@@ -6,31 +6,37 @@ define([
 
     app.register.controller('LoginCtrl',['$scope', '$modal', '$location', 'cmLogger',
         function($scope, $modal, $location, cmLogger){
+            $scope.showLoginRhino = true;
+            $scope.showLoginScreenStart = true;
 
-        $scope.open = function () {
+            $scope.open = function () {
+                $scope.showLoginScreenStart = false;
+                $scope.showLoginRhino = false;
 
-            var modalInstance = $modal.open({
-                template: '<div cm-notify></div><div cm-login></div>',
-                controller: function ($rootScope, $scope, $modalInstance) {
-                    $rootScope.$on('cmLogin:success', function(){
-                        $modalInstance.close();
-                    })
-                }
-            });
+                var modalInstance = $modal.open({
+                    template: '<div cm-notify></div><div cm-login></div>',
+                    controller: function ($rootScope, $scope, $modalInstance) {
+                        $rootScope.$on('cmLogin:success', function(){
+                            $modalInstance.close();
+                        })
+                    }
+                });
 
-            modalInstance.result
-            .then(
-                function () {
+                modalInstance.result
+                .then(
+                    function () {
 
-                },
-                function () {
-                    cmLogger.info('Modal dismissed at: ' + new Date());
-                }
-            );
-        };
+                    },
+                    function () {
+                        $scope.showLoginScreenStart = true;
+                        $scope.showLoginRhino = true;
+//                        cmLogger.info('Modal dismissed at: ' + new Date());
+                    }
+                );
+            };
 
-        $scope.goToReg = function(){
-            $location.path('/registration');
-        };
+            $scope.goToReg = function(){
+                $location.path('/registration');
+            };
     }]);
 });
