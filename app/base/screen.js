cbee87099c9ca11d11ec43a5cadfee31d00f0fc9
rhotep <andreas.pittrich@posteo.de>
2014-03-17 15:58:43 +0100

    login auf em umgestellt

--- app/base/screen.js
+++ app/base/screen.js
@@ -1,7 +1,7 @@
 function initScreenWidth(){
-    var body = document.getElementsByTagName('body')[0]
+    var html = document.getElementsByTagName('html')[0]
 
-    body.style.fontSize = (window.innerWidth/30) +'px'
+    html.style.fontSize = (window.innerWidth/30) +'px'
 }
 
 initScreenWidth()
