ea9dbd31d9f35464388468db715ecd6a867f3956
Empujador <b.schaedel@xuffix.com>
2014-03-20 16:09:20 +0100

    height problems

--- app/comps/validate/password-drtv.js
+++ app/comps/validate/password-drtv.js
@@ -12,6 +12,15 @@ function cmPassword(cmCrypt) {
             $scope.showStrengthMeter = false;
             $scope.passwordType = 'password';
             $scope.showPassword = false;
+            $scope.showPasswordLengthError = false;
+
+            $scope.checkPasswordLength = function(pw){
+                if(pw.length > 0 && pw.length < 6){
+                    $scope.showPasswordLengthError = true;
+                }
+
+                $scope.showPasswordLengthError = false;
+            }
 
             $scope.togglePassword = function(){
                 if($scope.showPassword){
@@ -26,35 +35,27 @@ function cmPassword(cmCrypt) {
             $scope.checkPWStrength = function(){
                 var pw = $scope.pw;
 
-//                if(pw != undefined && pw.length > 3){w
                 if(pw != undefined){
+                    $scope.checkPasswordLength(pw);
+
                     $scope.showStrengthMeter= true;
                     var bits = passchk_fast.passEntropy(pw);
 
-                    console.log(bits);
+//                    console.log(bits);
 
                     if(bits < 28){
-                        //$scope.percent = 10;
-//                        $scope.percent = (10 / 100) * Math.floor(bits);
                         $scope.color = '#d9534f';
                         //very weak
                     } else if(bits < 36){
-                        //$scope.percent = 25;
-//                        $scope.percent = 25 + (25 / 100) * Math.floor(bits);
                         $scope.color = '#f0ad4e';
                         //weak
                     } else if(bits < 60){
-                        //$scope.percent = 50;
-//                        $scope.percent = 50 + (50 / 100) * Math.floor(bits);
                         $scope.color = '#f0df43';
                         //reasonable || normal
                     } else if(bits < 128){
-                        //$scope.percent = 75;
-//                        $scope.percent = 75 + (75 / 100) * Math.floor(bits);
                         $scope.color = '#c4f04e';
                         //strong
                     } else {
-                        //$scope.percent = 100;
                         $scope.color = '#5cb85c';
                         //very strong
                     }
@@ -63,7 +64,7 @@ function cmPassword(cmCrypt) {
                     //100*Math.max(0,(1-Math.pow(1.4,((bits-10)*-0.08)))) 
                     //100*bits / Math.max(128, bits)
 
-                    $scope.pwStrength = pw;
+//                    $scope.pwStrength = pw;
                 } else {
                     $scope.percent = 0;
                     $scope.color = '#d9534f';
