1b58bfa8db33c811e5663a503708203d9538846b
rhotep <andreas.pittrich@posteo.de>
2014-03-24 14:25:53 +0100

    variablen korrekt benannt

--- app/base/screen.js
+++ app/base/screen.js
@@ -1,8 +1,8 @@
 function initScreenWidth(){
     var html    = document.getElementsByTagName('html')[0],
         app     = document.getElementById('cm-app')
-        height  = window.innerWidth,
-        width   = window.innerHeight,
+        width   = window.innerWidth,
+        height  = window.innerHeight,
         effectiveWidth = Math.min(height, width)
 
     html.style.fontSize = (effectiveWidth/32) +'px'
