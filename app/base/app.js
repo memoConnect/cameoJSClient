bff82d0634cfda0666060522b41909cab65a962a
rhotep <andreas.pittrich@posteo.de>
2014-03-25 14:29:32 +0100

    nkl

--- app/base/app.js
+++ app/base/app.js
@@ -167,7 +167,7 @@ define([
             };
 
             $rootScope.$on('$routeChangeSuccess', function(schmu, _currentRoute_, _prevRoute_){
-                if(!_currentRoute_.$$route) return null;
+                if(!_currentRoute_.$$route || !_currentRoute_.$$route.originalPath) return null;
 
                 var currentRoute = _currentRoute_.$$route.originalPath,
                     prevRoute = _prevRoute_ ? _prevRoute_.$$route.originalPath: '';
