a2f846f5a44f480d63f7f72796fd8548f65fd597
Empujador <b.schaedel@xuffix.com>
2014-03-25 18:01:54 +0100

    add identity avatar mock

--- app/shared/ui/avatar-drtv.js
+++ app/shared/ui/avatar-drtv.js
@@ -1,6 +1,13 @@
 function cmAvatar(){
     return {
         restrict : 'AE',
-        template : '<i class="fa cm-person"></i>'
+//        template : '<i class="fa cm-person"></i>',
+        link: function($scope, $element, $attrs){
+            var identity = $scope.$eval($attrs.cmData);
+            if(identity){
+                $element.css({'background-image': 'url(' + identity.getAvatar() +')','background-size':'cover'});
+            }
+        }
+
     }
 }
