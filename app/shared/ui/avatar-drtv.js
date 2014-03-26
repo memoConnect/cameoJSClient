2a7ef8a2e708317222ad760fc3e3841984b2aa97
Empujador <b.schaedel@xuffix.com>
2014-03-26 11:53:31 +0100

    bugifx avatar loading in messages

--- app/shared/ui/avatar-drtv.js
+++ app/shared/ui/avatar-drtv.js
@@ -5,9 +5,8 @@ function cmAvatar(){
         link: function($scope, $element, $attrs){
             var identity = $scope.$eval($attrs.cmData);
             if(identity){
-//                $element.css({'background-image': 'url(' + identity.getAvatar() +')','background-size':'cover'});
+                $element.css({'background-image': 'url(' + identity.getAvatar() +')','background-size':'cover'});
             }
         }
-
     }
 }
