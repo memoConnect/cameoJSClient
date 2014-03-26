91502d2b1937e6447433aeebe65a186ab116275b
Andreas Pittrich <andreas.pittrich@web.de>
2014-03-26 16:19:41 +0100

    header und footer für contacts ubersicht angelegt

--- test/e2e/config-e2e-tests.js
+++ test/e2e/config-e2e-tests.js
@@ -1,3 +1,3 @@
 this.wwwUrl = 'http://localhost:9000/app/';
-this.accountName = 'Z0dUE6IQ';
-this.accountPassword = 'password';
+this.accountName = 'Z1395672127689';
+this.accountPassword = 'PWD_Z1395672127689';
