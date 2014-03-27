55fa6dec6e9e21c78adc6a09abbf362ea9b31ce8
root <root@play2-dev02.(none)>
2014-03-27 15:15:41 +0000

    bugfix

--- dockerHost/update.sh
+++ dockerHost/update.sh
@@ -7,7 +7,7 @@ fi
 
 # stop existing container
 container=$(sudo docker ps | grep $1 | cut -f1 -d' ')
-if [ ! -z container ]; then
+if [ ! -z ${container} ]; then
    sudo docker stop ${container}
 fi
 
