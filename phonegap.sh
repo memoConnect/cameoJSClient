d9fd2ca5042ebe90701bcc24f81a27c72a480210
Reimerei <git@bjrm.de>
2014-03-27 17:32:20 +0100

    updated scripts

--- phonegap.sh
+++ phonegap.sh
@@ -2,23 +2,23 @@
 ./setup.sh 
 
 if [ ! -z "$1" ]; then
-	echo -e "\e[33m[cameo - connecting to adb device at $1:5555]\033[0m"
+	echo -e "\e[33m[ CameoClient - connecting to adb device at $1:5555 ]\033[0m"
 	adb connect $1:5555
 fi
 
 device=$(adb devices | head -2 | tail -1)
 
 if [ -z "$device" ]; then
-	echo -e "\e[33m[cameo - no device found]\033[0m"
+	echo -e "\e[33m[ CameoClient - no device found ]\033[0m"
 	exit 1
 else
-	echo -e "\e[33m[cameo - deploying after build to device: $device]\033[0m"
+	echo -e "\e[33m[ CameoClient - deploying after build to device: $devic e]\033[0m"
 fi
 
 echo -e "\e[33m[cameo - build phonegap]\033[0m"
 # do grunt task to make some magic
 ./node_modules/grunt-cli/bin/grunt phonegap
 
-echo -e "\e[33m[cameo - compile phonegap and install on: $device]\033[0m"
+echo -e "\e[33m[ CameoClient - compile phonegap and install on: $device ]\033[0m"
 # change to buildet dir and compile .apk
 ./node_modules/grunt-cli/bin/grunt phonegap:run
