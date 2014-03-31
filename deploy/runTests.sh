c1b92f32cc209e85435411c1cbd13f86e4218107
Reimerei <git@bjrm.de>
2014-03-31 15:21:59 +0200

    updated scripts

--- deploy/runTests.sh
+++ deploy/runTests.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+imageName="cameo-test"
 
-sudo docker build -t cameo-test . 
-containerId=$(sudo docker run -d -p 9000:9000 cameo-test)
+sudo docker build --no-cache -t ${imageName} --rm . 
+sudo docker run -p 9000:9000 -d ${imageName}
+
+containerId=$(sudo docker ps | grep ${imageName} | cut -f1 -d' ')
+
 echo ContainerId: ${containerId}
 
-while [ $( sudo docker logs ${containerId} | tail -n1 || grep Listening) ]; do
+while [ -z "${log}" ]; do
 	echo waiting for container to start
+	log=$(sudo docker logs ${containerId} | grep Listening)
+	echo $(sudo docker logs ${containerId} | tail -n1)
+	sleep 2
 done
+
+sudo docker stop ${containerId}
