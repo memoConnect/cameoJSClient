aafe30f7e8f8f3d724fd4ef2dea70f89c38b24d1
rhotep <andreas.pittrich@posteo.de>
2014-04-01 17:21:13 +0200

    tests ergänzt

--- test/unit/shared/cmNotify.fix.spec.js
+++ test/unit/shared/cmNotify.fix.spec.js
@@ -1,77 +1,103 @@
 'use strict';
 
 describe("cmNotify", function() {
-    var cmNotify,
-        el,
+    var el,
         scope,
+        cmNotify,
         $compile,
         $httpBackend,
         $http
 
     beforeEach(module("cmNotify"))
     beforeEach(inject(function(_cmNotify_, _$rootScope_, _$compile_, _$http_, _$httpBackend_){
-        cmNotify 	    = _cmNotify_
-        el				= angular.element('<div cm-notify></div>')
-        scope			= _$rootScope_.$new()
-        $compile		= _$compile_
-        $httpBackend	= _$httpBackend_
-        $http			= _$http_
-
-        $compile(el)(scope)
-        scope.$digest()
+        cmNotify        = _cmNotify_
+        $compile        = _$compile_
+        $httpBackend    = _$httpBackend_
+        $http           = _$http_
     }))
 
     it('should provide a service "cmNotify".', function(){
         expect(cmNotify).toBeDefined()
     })
+    
 
-    it('should have a template', function(){
-        expect(el.html()).not.toBe('')
-    })
+    describe("cmNotify directive", function() {
 
-    it('should display warnings.', function(){
-        cmNotify.warn('waring_1')
-        cmNotify.warn('waring_2')
-        cmNotify.warn('waring_3')
-        scope.$digest()
-        expect($('.growl',el).children().length).toEqual(3)
-    })
+        var el,scope
 
-    it('should display infos.', function(){
-        cmNotify.info('info_1')
-        cmNotify.info('info_2')
-        cmNotify.info('info_3')
-        scope.$digest()
-        expect($('.growl',el).children().length).toEqual(3)
-    })
+        beforeEach(inject(function(_cmNotify_, _$rootScope_, _$compile_, _$http_, _$httpBackend_){
+            el              = angular.element('<div cm-notify></div>')
+            scope           = _$rootScope_.$new()
 
-    it('should display success messages.', function(){
-        cmNotify.success('info_1')
-        cmNotify.success('info_2')
-        cmNotify.success('info_3')
-        scope.$digest()
-        expect($('.growl',el).children().length).toEqual(3)
-    })
+            $compile(el)(scope)
+            scope.$digest()
+        }))
 
-    it('should display error messages.', function(){
-        cmNotify.error('info_1')
-        cmNotify.error('info_2')
-        cmNotify.error('info_3')
-        scope.$digest()
-        expect($('.growl',el).children().length).toEqual(3)
-    })
-
-    it('should intercept messages in backend communication.', function(){
-        $httpBackend.whenGET('/any').respond({
-            "messages":	[
-                {text: 		"my warning", severity:	"warn"},
-                {text: 		"my error", severity:	"error"}
-            ]
+        it('should have a template', function(){
+            expect(el.html()).not.toBe('')
         })
 
-        $http.get('/any')
-        $httpBackend.flush();
+        it('should display warnings.', function(){
+            cmNotify.warn('waring_1')
+            cmNotify.warn('waring_2')
+            cmNotify.warn('waring_3')
+            scope.$digest()
+            expect($('.growl',el).children().length).toEqual(3)
+        })
 
-        expect($('.growl',el).children().length).toEqual(2)
+        it('should display infos.', function(){
+            cmNotify.info('info_1')
+            cmNotify.info('info_2')
+            cmNotify.info('info_3')
+            scope.$digest()
+            expect($('.growl',el).children().length).toEqual(3)
+        })
+
+        it('should display success messages.', function(){
+            cmNotify.success('success_1')
+            cmNotify.success('success_2')
+            cmNotify.success('success_3')
+            scope.$digest()
+            expect($('.growl',el).children().length).toEqual(3)
+        })
+
+        it('should display error messages.', function(){
+            cmNotify.error('error_1')
+            cmNotify.error('error_2')
+            cmNotify.error('error_3')
+            scope.$digest()
+            expect($('.growl',el).children().length).toEqual(3)
+        })
+
+        it('should intercept messages in backend communication.', function(){
+            $httpBackend.whenGET('/any').respond({
+                "messages":	[
+                    {text: 		"my warning", severity:	"warn"},
+                    {text: 		"my error", severity:	"error"}
+                ]
+            })
+
+            $http.get('/any')
+            $httpBackend.flush();
+
+            expect($('.growl',el).children().length).toEqual(2)
+        })
+    })
+
+    describe("cmNotifySignal directive", function() {
+        beforeEach(inject(function(_cmNotify_, _$rootScope_, _$compile_, _$http_, _$httpBackend_){
+            el              = angular.element('<cm-notify-signal></cm-notify-signal>')
+            scope           = _$rootScope_.$new()
+
+            $compile(el)(scope)
+            scope.$digest()
+        }))
+
+        it('should respond to notifications', function(){
+            expect(scope.unreadNotifications).toBe(false)
+            cmNotify.error('test_me')
+            expect(scope.unreadNotifications).toBe(true)
+        })
     })
 })
+
