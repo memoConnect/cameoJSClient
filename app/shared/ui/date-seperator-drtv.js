a0d1c9be3e025cd7acc9c4f002301b5c74dcdd92
dutscher <dutscher@cameo.io>
2014-04-01 16:46:01 +0200

    cm-menu fixxed to bottom and scrollable

--- app/shared/ui/date-seperator-drtv.js
+++ app/shared/ui/date-seperator-drtv.js
@@ -2,7 +2,7 @@ function cmDateSeperator(){
     return{
         restrict: 'AE',
         transclude: true,
-        template: '<div class="date-seperator">'+
+        template: '<div class="date-seperator" cm-rubber-space>'+
                     '<div class="line" cm-weight="1"></div>'+
                     '<div class="date" cm-weight="3" ng-transclude></div>'+
                     '<div class="line" cm-weight="1"></div>'+
