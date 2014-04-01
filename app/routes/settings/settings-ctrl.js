53014c8870dd0b1019eccf16e3af3aecf0e28a1d
dutscher <dutscher@cameo.io>
2014-04-01 17:05:29 +0200

    repair settings for keypair

--- app/routes/settings/settings-ctrl.js
+++ app/routes/settings/settings-ctrl.js
@@ -13,18 +13,8 @@ define([
         'cmUserModel',
         'cmLogger',
         'cmUtil',
-    function($scope, $rootScope, cmUserModel, cmLogger, cmUtil) {
+    function($scope, $rootScope, cmUserModel) {
         $scope.identity = cmUserModel.data;
-
-        /**
-         * Ctrl Tabs
-         * @type {*[]}
-         */
-        $scope.tabs = [
-            {i18n:'BACK',icon:'fa-chevron-left',href:'#/start'},
-            {i18n:'SPINNER',icon:'fa-spinner'},
-            {i18n:'KEY',icon:'fa-key','default':true}
-        ];
 
         /**
          * Spinner Tests
