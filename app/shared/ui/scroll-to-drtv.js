064fc76c99f8b4616ef02b6b017dfbaa1040006f
dutscher <dutscher@cameo.io>
2014-04-04 10:24:55 +0200

    fixxed scroll-to

--- app/shared/ui/scroll-to-drtv.js
+++ app/shared/ui/scroll-to-drtv.js
@@ -4,13 +4,20 @@ function cmScrollTo($timeout){
     return {
         restrict: 'A',
         link: function($scope, $element, $attrs){
-            if($scope.$last && $attrs.cmScrollTo != ''){
+
+            function initTimeout(){
                 var anchor = angular.element(document.querySelector($attrs.cmScrollTo)),
                     body = angular.element(document.querySelector('body'));
 
                 $timeout(function(){
                     body[0].scrollTop = anchor[0].offsetTop;
                 },500);
+            }
+
+            if($attrs.ngRepeat && $scope.$last && $attrs.cmScrollTo != ''){
+                initTimeout();
+            } else if(!$attrs.ngRepeat){
+                initTimeout();
             }
         }
     }
