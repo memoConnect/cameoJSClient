e8902a1cfbbb0683d9f38d9fd248d28aa91e13d5
Empujador <b.schaedel@xuffix.com>
2014-04-07 18:23:17 +0200

    fix error in test

--- test/unit/shared/cmUserModel.fix.spec.js
+++ test/unit/shared/cmUserModel.fix.spec.js
@@ -139,7 +139,7 @@ describe('cmUserModel', function(){
 
             decrypted_secret = cmUserModel.decryptPassphrase(encrypted_secret)
             expect(decrypted_secret).toBeFalsy()
-        })
+//        })
 
             cmUserModel.saveKey(good_key)
             decrypted_secret = cmUserModel.decryptPassphrase(encrypted_secret)
