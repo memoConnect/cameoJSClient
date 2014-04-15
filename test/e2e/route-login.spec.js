3717cfef0ef331a1cbc2e334f67b1d949c975397
Reimerei <git@bjrm.de>
2014-04-15 15:58:09 +0200

    wrote first tests

--- test/e2e/route-login.spec.js
+++ test/e2e/route-login.spec.js
@@ -1,5 +1,5 @@
 var config = require("./config-e2e-tests.js")
-var util = require("../lib/e2e/cmUtil.js")
+var util = require("../lib/e2e/cmTestUtil.js")
 
 describe('login screen', function () {
 
@@ -8,16 +8,85 @@ describe('login screen', function () {
     beforeEach(function () {
         ptor = protractor.getInstance();
         ptor.ignoreSynchronization = true;
-        browser.ignoreSynchronization = true;
+//        browser.ignoreSynchronization = true;
     });
 
 
     it('should contain two buttons', function () {
 
-        util.cmLoadPage(config.wwwUrl, ptor)
+        ptor.get(config.wwwUrl);
+        util.waitForPageLoad(ptor);
+        ptor.getCurrentUrl().then(function(url){
+            expect(url).toMatch(/\/\#\/login$/)
+        })
 
-        ptor.findElements(by.css("[data-qa]")).then(function(elements) {
+
+        $$("[data-qa]").then(function (elements) {
             expect(elements.length).toBe(2)
         })
     });
-});
+
+    it('should prompt for username and password after click on login and close it', function () {
+
+        $("[data-qa='login-btn']").click();
+
+        var user = $("input[name=user]");
+        var pw = $("input[name=pw]");
+
+        user.getAttribute("placeholder").then(function (text) {
+            expect(text).not.toBe("");
+        });
+        pw.getAttribute("placeholder").then(function (text) {
+            expect(text).not.toBe("");
+        });
+
+        $("body").sendKeys(protractor.Key.ESCAPE);
+
+        util.waitForModalClose(ptor)
+
+    });
+
+    it('should show error on wrong login', function () {
+
+        $("[data-qa='login-btn']").click();
+
+        var user = $("input[name=user]");
+        var pw = $("input[name=pw]");
+
+        user.sendKeys("moep");
+        pw.sendKeys("moep!");
+
+        $("[data-qa='login-submit-btn']").click();
+
+        util.waitForSpinner(ptor)
+
+
+        expect($("[data-qa='login-info']").isDisplayed()).toBe(true)
+        $("[data-qa='login-info']").getText().then(function (text) {
+            expect(text).not.toBe("")
+        })
+
+        $("body").sendKeys(protractor.Key.ESCAPE);
+
+        util.waitForModalClose(ptor)
+    })
+
+    it('should login with correct credentials', function () {
+
+        $("[data-qa='login-btn']").click();
+
+        var user = $("input[name=user]");
+        var pw = $("input[name=pw]");
+
+        user.sendKeys(config.existingLoginName);
+        pw.sendKeys(config.existingPassword);
+
+        $("[data-qa='login-submit-btn']").click();
+
+        util.waitForPageLoad(ptor)
+
+        ptor.getCurrentUrl().then(function(url){
+            expect(url).toMatch(/\/\#\/talks$/)
+        })
+    })
+})
