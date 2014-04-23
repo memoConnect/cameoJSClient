17d291c450e7fa0a9fbed93af76e87f0ba3cbbf4
Empujador <b.schaedel@xuffix.com>
2014-04-23 18:57:53 +0200

    set ttl to modal

--- app/comps/contacts/drtv-friend-request-list.js
+++ app/comps/contacts/drtv-friend-request-list.js
@@ -39,7 +39,7 @@ angular.module('cmContacts').directive('cmFriendRequestList',[
                             function(){
                                 cmContactsModel.removeFriendRequest(item);
 
-                                cmNotify.success('CONTACTS.INFO.REQUEST.ACCEPT');
+                                cmNotify.success('CONTACTS.INFO.REQUEST.ACCEPT',{ttl:2000});
 
                                 $rootScope.$broadcast('cmNotify:update');
 
