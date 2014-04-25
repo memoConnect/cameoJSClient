298081fc97e8134b9f1f91ef249e84a8fca8be77
dutscher <dutscher@cameo.io>
2014-04-25 11:42:01 +0200

    specs

---
+++ test/e2e/friendrequests.spec.js
@@ -0,0 +1,10 @@
+var config = require("./config-e2e-tests.js")
+var util = require("../lib/e2e/cmTestUtil.js")
+
+describe('Friendrequests', function () {
+
+    var userToAccept = 'moeperAccept_'+Date.now();
+
+    describe()
+
+})
