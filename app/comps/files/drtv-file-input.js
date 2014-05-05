c36f6d6aadeafd716213ebe27a98364039203fb0
dutscher <dutscher@cameo.io>
2014-05-05 12:49:47 +0200

    drtv upload in answer / ready for asset model intergration

--- app/comps/files/drtv-file-input.js
+++ app/comps/files/drtv-file-input.js
@@ -3,10 +3,10 @@
 angular.module('cmFiles').directive('cmFileInput', [
     function () {
         return {
-            restrict : 'AE',
-            require : '^cmUpload',
-            scope : {},
-            template : '<input type="file" placeholder ="{FILES.PLACEHOLDER.UPLOAD|cmTranslate}">',
+            restrict: 'AE',
+            require: '^cmUpload',
+            scope: {},
+            template: '<input type="file" placeholder="{FILES.PLACEHOLDER.UPLOAD|cmTranslate}">',
 
             link: function (scope, element, attributes, uploadCtrl) {
                 element.on("change", function (event) {
