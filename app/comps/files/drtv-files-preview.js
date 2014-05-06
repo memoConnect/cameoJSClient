92de025cecab8df4771864f024980cc3af2d8da5
dutscher <dutscher@cameo.io>
2014-05-06 14:34:05 +0200

    preload files in drtv-message

--- app/comps/files/drtv-files-preview.js
+++ app/comps/files/drtv-files-preview.js
@@ -8,6 +8,10 @@ angular.module('cmFiles').directive('cmFilesPreview',[
             templateUrl: 'comps/files/drtv-files-preview.html',
             controller: function($scope){
                 $scope.cmFileTypes = cmFileTypes;
+
+                $scope.isImage = function(mime){
+                    return mime == undefined ? false : mime.search('^image/') != -1;
+                };
             }
         }
     }
