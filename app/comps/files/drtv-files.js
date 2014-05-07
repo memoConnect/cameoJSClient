01f5f2f6c52553120ffab3d17b67c59161e481ab
Empujador <b.schaedel@xuffix.com>
2014-05-07 12:33:58 +0200

    clean up

--- app/comps/files/drtv-files.js
+++ app/comps/files/drtv-files.js
@@ -8,32 +8,6 @@ angular.module('cmFiles').directive('cmFiles',[
             restrict : 'E',
             controller : function($scope, $element, $attrs){
 
-//                var self = this,
-//                    file = new cmFile()
-//
-//                $scope.progress = 0
-//                $scope.readyForUpload = undefined
-//
-//                $scope.chunkSize = $scope.$eval($attrs.cmChunkSize)
-//                $scope.$parent.$watch($attrs.cmChunkSize, function(new_chunk_size){ $scope.chunkSize = new_chunk_size })
-//
-//                $scope.passphrase = $scope.$parent.$eval($attrs.cmPassphrase)
-//                $scope.$parent.$watch($attrs.cmPassphrase, function(passphrase){ $scope.passphrase = passphrase })
-//
-//                $scope.fileId = $scope.$parent.$eval($attrs.ngModel)
-//                $scope.$parent.$watch($attrs.ngModel, function(ngModel){ $scope.fileId = ngModel })
-//
-//                $scope.upload = function(){
-//                    $scope.progress = 0
-//                    if(!$scope.readyForUpload) return null
-//
-//                    $scope.readyForUpload.then(function(){
-//                        file
-//                            .uploadChunks()
-//                            .then(null, null, function(progress){ $scope.progress += progress })
-//                    })
-//                }
-
                 $scope.files = [];
                 /**
                  * function called via <input type=file>
