9be0916fe382b6352d52aa1f3ad8b26e3b1fa438
rhotep <andreas.pittrich@posteo.de>
2014-05-26 17:48:01 +0200

    tests für cmFactory angefangen

---
+++ test/unit/shared/core/files/cmFactory.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+describe('cmFactory', function(){
+
+    var factory
+
+
+    function TestModel(data){
+        this.data = data
+        this.id = data.id
+    }
+
+    beforeEach(module('cmCore'))
+    beforeEach(inject(function(_cmFactory_){
+        factory = new _cmFactory_(TestModel)
+    }))
+
+    it('should provide a function to create new instances of assigned model or retrieve an existing one.', function(){
+
+        var instance_count  = 0
+
+        factory.on('register', function(instance){ instance_count++ })
+
+        var instance        = factory.create({id: 'my_id'})           
+        
+        expect(instance instanceof TestModel).toBe(true)
+        expect(instance.id).toBe('my_id')
+        expect(factory.length).toBe(1)
+        expect(instance_count).toBe(1)
+
+        var instance2       = factory.create({id: 'my_id'})
+
+        expect(instance).toBe(instance2)
+        expect(instance2.id).toBe('my_id')
+        expect(factory.length).toBe(1)
+        expect(instance_count).toBe(1)
+
+    })
+
+})
