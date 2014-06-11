15d98a12e114d2b08b139928dcc85e0ab6dd5cee
dutscher <dutscher@cameo.io>
2014-06-11 17:31:12 +0200

    fixxed filter on message

--- app/comps/ui/fltr-inline-translate.js
+++ app/comps/ui/fltr-inline-translate.js
@@ -5,14 +5,19 @@ angular.module('cmUi').filter('cmInlineTranslate', [
     '$filter',
 
     function($filter){
-        return function(text){ 
+        return function(text){
+
+            if(text == undefined)
+                return '';
+
             var result = text,
-                matches = text.match(/\$\$\{[A-Z0-9\.]*\}/) || []
+                matches = text.match(/\$\$\{[A-Z0-9\.]*\}/) || [];
 
             matches.forEach(function(match){
                 result = result.replace(match, $filter('cmTranslate')(match.replace('$${','').replace('}','')))
-            })
-            return result
+            });
+
+            return result;
         }
     }
 ]);
--- app/comps/ui/fltr-parse.js
+++ app/comps/ui/fltr-parse.js
@@ -7,6 +7,7 @@ angular.module('cmUi').filter('cmParse', [
     function($filter){
         return function(text){
 
+            text = $filter('cmTranslate')(text)
             text = $filter('cmInlineTranslate')(text)
             text = $filter('cmAutolink')(text, 40)
             text = $filter('nl2br')(text)
