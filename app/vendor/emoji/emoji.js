baa274fc847f3a15f972b12a616341c06b6ef79a
dutscher <dutscher@cameo.io>
2014-06-11 16:00:46 +0200

    added emojis to textarea/ngModel

--- app/vendor/emoji/emoji.js
+++ app/vendor/emoji/emoji.js
@@ -167,12 +167,77 @@
         ],
         rEmojis = new RegExp(":(" + emojis.join("|") + "):", "g");
 
-    angular.module("emoji", []).filter("emoji", function () {
-        return function (input) {
+    angular.module('emoji',['cmCore'])
+    .filter('emoji', function(){
+        return function (input){
             return input.toString().replace(rEmojis, function (match, text) {
-                return "<i class='emoji emoji_" + text + "' title=':" + text + ":'>" + text + "</i>";
+                return '<i class="emoji emoji_'+text+'" title=":'+text+':">'+text+'</i>';
             });
         };
-    });
+    })
+    .directive('cmEmojiList', function(cmUtil){
+        return{
+            restrict: 'E',
+            template: '<div ng-show="showList"><div>',
+            link: function(scope, element, attrs){
 
+                var textarea = undefined,
+                    textareaModel = undefined;
+
+                scope.showList = false;
+
+                scope.toggleList = function(){
+                    scope.showList = scope.showList ? false : true;
+                    scope.$apply();
+                };
+
+                scope.insertEmoji = function(emoji){
+                    if(textarea != undefined && textarea.length > 0){
+                        scope.insertAt(':'+emoji+':');
+                    }
+                };
+
+                scope.insertAt = function(text){
+                    var oldValue = textarea.val(),
+                        textStart = textarea[0].selectionStart,
+                        textEnd = textarea[0].selectionEnd,
+                        insertSymbol = text+' ',
+                        strWithEmoticon = oldValue.substring(0, textStart);
+                        strWithEmoticon+= cmUtil.endsWith(strWithEmoticon, ' ') ? insertSymbol : ' '+insertSymbol;
+                        strWithEmoticon+= oldValue.substring(textEnd);
+
+                    scope.showList = false;
+
+                    if(textareaModel != undefined) {
+                        textarea.val(strWithEmoticon);
+                        scope[textareaModel] = strWithEmoticon;
+                    }
+
+                    textarea[0].focus();
+                    textarea[0].selectionStart = textEnd+insertSymbol.length;
+                    textarea[0].selectionEnd = textEnd+insertSymbol.length;
+                };
+
+                // create emojis
+                emojis.forEach(function(emoji){
+                    var icon = angular.element('<i class="emoji emoji_'+emoji+'" title=":'+emoji+':">'+emoji+'</i>');
+                        icon.on('click',function(){scope.insertEmoji(emoji)});
+                    element.children().append(icon);
+                });
+                // visiblitity handler
+                if(attrs.cmHandler){
+                    var handler = angular.element(document.getElementById(attrs.cmHandler));
+                    handler.on('click',function(){scope.toggleList()});
+                }
+                // textarea for emoji insertion
+                if(attrs.cmTextarea){
+                    textarea = angular.element(document.getElementById(attrs.cmTextarea));
+
+                    if(textarea.length > 0) {
+                        textareaModel = textarea.attr('ng-model');
+                    }
+                }
+            }
+        }
+    });
 }());
