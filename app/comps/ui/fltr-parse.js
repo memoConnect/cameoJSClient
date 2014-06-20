8f2ddfb1ab67866c06e8e490051a5678985e1e59
dutscher <dutscher@cameo.io>
2014-06-20 03:40:28 +0200

    emojis resolved and categorized

--- app/comps/ui/fltr-parse.js
+++ app/comps/ui/fltr-parse.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('cmUi').filter('cmParse', [
-
     '$filter',
-
     function($filter){
         return function(text){
 
-            text = $filter('cmTranslate')(text)
-            text = $filter('cmInlineTranslate')(text)
-            text = $filter('cmAutolink')(text, 40)
-            text = $filter('nl2br')(text)
-            text = $filter('emoji')(text)
+            text = $filter('cmTranslate')(text);
+            text = $filter('cmInlineTranslate')(text);
+            text = $filter('cmAutolink')(text, 40);
+            text = $filter('cmEmoji')(text);
+            text = $filter('nl2br')(text);
 
-            return text
+            return text;
         }
     }
 ]);
