18a8125edf000b093e17dfd717379dcb4a3431f1
Empujador <b.schaedel@xuffix.com>
2014-06-23 17:46:41 +0200

    update identity gui

--- app/routes/settings/comps/drtv-identity-settings.js
+++ app/routes/settings/comps/drtv-identity-settings.js
@@ -2,12 +2,30 @@
 
 angular.module('cmRouteSettings').directive('cmIdentitySettings', [
     'cmUserModel',
-    function(cmUserModel){
+    '$location',
+    function(cmUserModel, $location){
         return {
             restrict: 'E',
             templateUrl: 'routes/settings/comps/drtv-identity-settings.html',
             controller: function ($scope) {
-                $scope.identity = cmUserModel.data;
+                $scope.identity = cmUserModel.data.identity;
+
+                console.log($scope.identity)
+
+                //////////////////////
+                // TODO: mock workarround json in array
+                $scope.identity.phoneNumbers = [
+                    $scope.identity.phoneNumber || {value:''}
+                ];
+                $scope.identity.emails = [
+                    $scope.identity.email || {value:''}
+                ];
+                //////////////////////
+//                console.log($scope.identity)
+
+                $scope.goToKeys = function(){
+                    $location.path('/settings/identity/keys');
+                }
             }
         }
     }
