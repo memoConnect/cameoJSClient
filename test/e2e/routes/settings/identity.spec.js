37678b6fd687e4725f2a6910b1d836589822774f
Empujador <b.schaedel@xuffix.com>
2014-06-23 19:22:42 +0200

    update test and ui

---
+++ test/e2e/routes/settings/identity.spec.js
@@ -0,0 +1,26 @@
+var config = require("./../../config-e2e-tests.js")
+var util = require("../../../lib/e2e/cmTestUtil.js")
+
+describe('Route: Settings - Identity', function(){
+    "use strict";
+
+    var ptor = util.getPtorInstance()
+
+    it('should be load at "#/settings/identity" after login.', function(){
+        util.login()
+        util.expectCurrentUrl('#/talks')
+
+        util.get('/settings/identity')
+        util.expectCurrentUrl('#/settings/identity')
+    })
+
+    it('should exists a key management button', function(){
+        util.waitForElement("[data-qa='btn-identity-keys']");
+        expect($("[data-qa='btn-identity-keys']").isPresent()).toBe(true)
+    })
+
+    it('should be exists a trust button', function(){
+        util.waitForElement("[data-qa='btn-identity-trust']");
+        expect($("[data-qa='btn-identity-trust']").isPresent()).toBe(true)
+    })
+});
