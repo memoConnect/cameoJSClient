1383b4dda2cb845b358703ae3936f2cbc442a031
Empujador <b.schaedel@xuffix.com>
2014-06-23 18:05:58 +0200

    rm old test

--- test/e2e/route-settings.spec.js
+++ test/e2e/route-settings.spec.js
@@ -36,12 +36,4 @@ describe('settings', function(){
         $("cm-menu li.has-submenu .cm-list-parent-label").click()
         util.expectCurrentUrl('#/settings')
     })
-
-    it('open one of overview goes to mainPage /settings/identity and click on subPage button / settings/identity/keys', function(){
-        $$("cm-overview-settings ul li").get(1).$("a").click()
-        util.expectCurrentUrl('#/settings/identity')
-        $$("cm-identity-settings ul li").get(0).$("a").click()
-        util.expectCurrentUrl('#/settings/identity/keys')
-    })
-
 })
