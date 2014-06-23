80033064b87829a3bdf177e5a9f3fccc9ba5ec05
Empujador <b.schaedel@xuffix.com>
2014-06-23 19:11:28 +0200

    rm old test

---
+++ test/e2e/routes/route-settings-identity.spec.js
@@ -0,0 +1,8 @@
+var config = require("./../config-e2e-tests.js")
+var util = require("../../lib/e2e/cmTestUtil.js")
+
+describe('Route: Settings - Identity', function(){
+    "use strict";
+
+    var ptor = util.getPtorInstance()
+});
