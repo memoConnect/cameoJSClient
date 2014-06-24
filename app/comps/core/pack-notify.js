19746cb31f468d110fbe40fbf60290eaa2f087bf
Empujador <b.schaedel@xuffix.com>
2014-06-24 20:26:28 +0200

    update

--- app/comps/core/pack-notify.js
+++ app/comps/core/pack-notify.js
@@ -4,33 +4,65 @@ angular.module('cmCore')
 .factory('cmNotifyModel', [
     'cmStateManagement',
     'cmObject',
+    'cmUtil',
     'cmLogger',
-    function(cmStateManagement, cmObject, cmLogger){
+    function(cmStateManagement, cmObject, cmUtil, cmLogger){
         function cmNotifyModel(){
             var self = this;
 
             cmObject.addEventHandlingTo(this);
-            this.state = new cmStateManagement(['new','read']);
+            this.state = new cmStateManagement(['new','read','error']);
 
+            this.id = undefined;
+            this.severity = undefined;
+            this.displayType = undefined;
+            this.callbackRoute = undefined;
+            this.bell = false;
 
+            /**
+             * {
+                "id": TRANSLATION.KEY
+                "severity: "info || warn || error || ..."
+                "displayType: "modal || banner || none"
+                "callbackRoute": "/conversation/id"
+                }
+             * @param data
+             */
             function init(data){
                 cmLogger.debug('cmNotifyModel.init');
 
                 self.state.set('new');
 
-                if(typeof data != undefined){
+                if(typeof data !== 'undefined'){
                     self.importData(data);
                 }
             }
 
             this.importData = function(data){
                 cmLogger.debug('cmNotifyModel.importData');
-
                 console.log('importData', data);
+
+                if(typeof data == 'object' || typeof data == 'array'){
+                    this.id = data.id || this.id;
+
+                    this.severity = data.severity || this.severity;
+
+                    this.displayType = data.displayType || this.displayType;
+
+                    this.callbackRoute = data.callbackRoute || this.callbackRoute;
+
+                    this.bell = data.bell || this.bell;
+                } else {
+                    this.state.set('error');
+                }
 
                 self.state.unset('new');
                 this.trigger('update:finished');
-            }
+            };
+
+            this.render = function(){
+
+            };
         }
 
         return cmNotifyModel;
