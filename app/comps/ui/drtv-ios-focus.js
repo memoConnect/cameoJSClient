a8f49eae477e93f76e1248e16aa85a7d9e412b48
dutscher <dutscher@cameo.io>
2014-06-25 15:57:00 +0200

    conversation controls viewport checkHeight ready to use.

--- app/comps/ui/drtv-ios-focus.js
+++ app/comps/ui/drtv-ios-focus.js
@@ -12,10 +12,10 @@ angular.module('cmUi').directive('cmIosFocus',[
                 var settings = undefined,
                     fixedElements = undefined,
                     view = undefined,
-                    trigger = undefined;
+                    handler = undefined;
 
                 function stopEvent(e){
-                    if(e.target != element[0] && e.target != trigger) {
+                    if(e.target != element[0] && e.target != handler) {
                         element[0].blur();
                         e.preventDefault();
                         e.stopPropagation();
@@ -28,10 +28,10 @@ angular.module('cmUi').directive('cmIosFocus',[
                     settings = scope.$eval(attrs.cmIosFocus),
                     fixedElements = angular.element($document[0].querySelectorAll(settings.fixedElements)),
                     view = angular.element($document[0].querySelectorAll('body,html')),
-                    trigger = $document[0].querySelector('.post-wrap');
+                    handler = $document[0].querySelector(settings.handler);
 
                     element.on('focus', function(event){
-                        $rootScope.$broadcast('cmIosFocus:focus');
+                        $rootScope.$broadcast('cmIosFocus:focus',element);
                         view.on('touchstart',stopEvent);
                         fixedElements.css('position','absolute');
                         if('scrollTop' in settings && settings.scrollTop) {
@@ -45,6 +45,13 @@ angular.module('cmUi').directive('cmIosFocus',[
                         $rootScope.$broadcast('cmIosFocus:blur');
                         fixedElements.css('position','fixed');
                         view.off('touchstart',stopEvent);
+                    });
+                } else {
+                    element.on('focus', function(event){
+                        $rootScope.$broadcast('cmIosFocus:focus',element);
+                    });
+                    element.on('blur', function(event){
+                        $rootScope.$broadcast('cmIosFocus:blur');
                     });
                 }
             }
