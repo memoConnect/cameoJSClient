e4edf97f94da753cb6f8de8f1a857591a50e3a31
dutscher <dutscher@cameo.io>
2014-07-02 18:53:24 +0200

    fixxed all header in all routes

--- app/comps/ui/drtv-header-list-search.js
+++ app/comps/ui/drtv-header-list-search.js
@@ -3,20 +3,27 @@
 angular.module('cmUi')
 .directive('cmHeaderListSearch',[
     '$rootScope',
-    function ($rootScope){
+    '$timeout',
+    function ($rootScope, $timeout){
         return {
             restrict: 'E',
             scope: {
                 ngModel: '=ngModel'
             },
-            template: '<i class="fa cm-search-cl" ng-click="toggleInput()"></i>' +
+            template: '<i class="fa cm-search" ng-click="toggleInput()"></i>' +
                       '<div ng-show="visible">' +
-                        '<cm-search-input ng-model="ngModel"></cm-search-input>' +
+                        '<cm-search-input ng-model="ngModel" cm-without-search-icon="true"></cm-search-input>' +
                       '</div>',
             controller: function($scope, $element, $attrs){
                 $scope.visible = false;
                 $scope.toggleInput = function(){
                     $scope.visible = $scope.visible ? false : true;
+                    // set focus to input
+                    if($scope.visible){
+                        $timeout(function(){
+                            $element[0].querySelector('input').focus();
+                        },50);
+                    }
                 }
             }
         }
--- app/comps/ui/drtv-search-input.js
+++ app/comps/ui/drtv-search-input.js
@@ -7,10 +7,38 @@ angular.module('cmUi').directive('cmSearchInput',[
             scope: {
                 search: '=ngModel'
             },
-            template:   '<input type="text" value="" ng-model="search" placeholder="{{placeholder}}" data-qa="input-search">' +
-                        '<i class="fa cm-search-cl"></i>',
+            template: '<input type="text" value="" ng-model="search" placeholder="{{placeholder}}" data-qa="input-search">' +
+                      '<i class="fa" ng-click="clear()" ng-class="{\'cm-search\':showDefaultIcon && counterKeydown == 0,\'cm-checkbox-wrong\':counterKeydown > 0}"></i>',
             link: function(scope, element, attrs){
                 scope.placeholder = attrs.placeholder || '';
+
+                element
+                .on('focus', function(){
+                    scope.counterKeydown = 0;
+                })
+                .on('keydown', function(){
+                    scope.counterKeydown++;
+                })
+                .on('keyup', function(){
+                    if(scope.search == ''){
+                        scope.counterKeydown = 0;
+                        scope.$apply();
+                    }
+                });
+            },
+            controller: function($scope, $element, $attrs){
+                $scope.counterKeydown = 0;
+
+                $scope.showDefaultIcon = true;
+
+                if('cmWithoutSearchIcon' in $attrs && $attrs.cmWithoutSearchIcon){
+                    $scope.showDefaultIcon = false
+                }
+
+                $scope.clear = function(){
+                    $scope.search = '';
+                    $scope.counterKeydown = 0;
+                }
             }
         }
     }
