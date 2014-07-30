46ca99a2ce0d57134f086a63196e647b1df08f1c
Empujador <b.schaedel@xuffix.com>
2014-07-30 16:55:46 +0200

    clean directive

--- app/routes/settings/comps/drtv-identity-first-key.js
+++ app/routes/settings/comps/drtv-identity-first-key.js
@@ -2,16 +2,13 @@
 
 angular.module('cmRouteSettings').directive('cmIdentityFirstKey', [
     'cmUserModel', 'cmCrypt', 'cmUtil', 'cmLogger', 'cmNotify', 'cmKey',
-    '$window','cmAuth',
+    '$window',
     function(cmUserModel, cmCrypt, cmUtil, cmLogger, cmNotify, cmKey,
              $window,cmAuth){
         return {
             restrict: 'E',
             templateUrl: 'routes/settings/comps/drtv-identity-first-key.html',
             controller: function ($scope) {
-                $scope.startBroadcast = function(){
-                    cmAuth.sendBroadcast({name:'broadcast:started',data:{msg:$scope.message}})
-                }
             }
         }
     }
