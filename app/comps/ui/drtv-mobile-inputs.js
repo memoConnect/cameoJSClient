d147537ac3f301d00c6670a1230a79ed63680a7a
dutscher <dutscher@cameo.io>
2014-07-31 20:59:41 +0200

    resolved mobile inputs that hides cm-header and cm-footer

--- app/comps/ui/drtv-mobile-inputs.js
+++ app/comps/ui/drtv-mobile-inputs.js
@@ -26,6 +26,57 @@
  handler = undefined;
 
  function stopEvent(e){
+    if(e.target != element[0] && e.target != handler) {
+        element[0].blur();
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+}
+
+ // Fix mobile floating toolbar when input is focused
+ if(cmEnv.isiOS && attrs.cmIosFocus != ''){
+    settings = scope.$eval(attrs.cmIosFocus),
+    fixedElements = angular.element($document[0].querySelectorAll(settings.fixedElements)),
+    view = angular.element($document[0].querySelectorAll('body,html')),
+    handler = $document[0].querySelector(settings.handler);
+
+    element.on('focus', function(event){
+        view.on('touchstart',stopEvent);
+        fixedElements.css('position','absolute');
+        if('scrollTop' in settings && settings.scrollTop) {
+            angular.forEach(view, function (tag) {
+                tag.scrollTop = 0;
+            });
+        }
+    });
+
+    element.on('blur', function(event){
+        fixedElements.css('position','fixed');
+        view.off('touchstart',stopEvent);
+    });
+}
+ */
+
+angular.module('cmUi').directive('input',[
+    'cmEnv',
+    '$document',
+    function (cmEnv,
+              $document) {
+
+        return {
+            restrict: 'EA',
+            link: function (scope, element) {
+                // only for mobile devices
+                if(cmEnv.isNotMobile)
+                    return false;
+
+                // mobile device? go on!
+                var tagName = element[0].tagName.toLowerCase(),
+                    fixedElements = angular.element($document[0].querySelectorAll('cm-header, cm-footer')),
+                    view = angular.element($document[0].querySelectorAll('body,html'));
+
+                function stopEvent(e){
                     if(e.target != element[0] && e.target != handler) {
                         element[0].blur();
                         e.preventDefault();
@@ -34,55 +85,31 @@
                     }
                 }
 
- // Fix mobile floating toolbar when input is focused
- if(cmEnv.isiOS && attrs.cmIosFocus != ''){
-                    settings = scope.$eval(attrs.cmIosFocus),
-                    fixedElements = angular.element($document[0].querySelectorAll(settings.fixedElements)),
-                    view = angular.element($document[0].querySelectorAll('body,html')),
-                    handler = $document[0].querySelector(settings.handler);
-
-                    element.on('focus', function(event){
-                        $rootScope.$broadcast('cmIosFocus:focus',element);
-                        view.on('touchstart',stopEvent);
-                        fixedElements.css('position','absolute');
-                        if('scrollTop' in settings && settings.scrollTop) {
-                            angular.forEach(view, function (tag) {
-                                tag.scrollTop = 0;
-                            });
-                        }
-                    });
-
-                    element.on('blur', function(event){
-                        $rootScope.$broadcast('cmIosFocus:blur');
-                        fixedElements.css('position','fixed');
-                        view.off('touchstart',stopEvent);
-                    });
-                } else {
-                    element.on('focus', function(event){
-                        $rootScope.$broadcast('cmIosFocus:focus',element);
-                    });
-                    element.on('blur', function(event){
-                        $rootScope.$broadcast('cmIosFocus:blur');
+                function onFocus(){
+                    view.on('touchstart',stopEvent);
+                    fixedElements.css('position','absolute');
+                    angular.forEach(fixedElements,function(fixedElement){
+                        if(fixedElement.tagName.toLowerCase() == 'cm-footer')
+                            angular.element(fixedElement).addClass('ng-hide');
                     });
                 }
 
+                function onBlur(){
+                    fixedElements.css('position','fixed');
+                    angular.forEach(fixedElements,function(fixedElement){
+                        if(fixedElement.tagName.toLowerCase() == 'cm-footer')
+                            angular.element(fixedElement).removeClass('ng-hide');
+                    });
+                    view.off('touchstart',stopEvent);
+                }
 
- */
-
-angular.module('cmUi').directive('input',[
-    'cmEnv',
-    '$document', '$rootScope',
-    function (cmEnv,
-              $document, $rootScope) {
-        return {
-            restrict: 'EA',
-            link: function (scope, element, attrs) {
-                var tagName = element[0].tagName.toLowerCase();
-
-                if(tagName == 'input' && element.attr('type') == 'text' ||// only text inputs
-                   tagName == 'textarea'
+                if(tagName == 'input' && element.attr('type') != 'checkbox' ||// only inputs text,password,mail,tel,
+                   tagName == 'textarea' // accept texareas to
                 ){
-                    console.log('do that shit baby',element[0])
+                    element.on('focus', onFocus);
+                    element.on('blur', onBlur);
+
+                    scope.$on('$destroy', onBlur);
                 }
             }
         }
