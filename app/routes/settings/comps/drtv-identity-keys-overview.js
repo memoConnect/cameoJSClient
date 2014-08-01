a952db226c13a7624ebd248297c83a3794e0c575
dutscher <dutscher@cameo.io>
2014-08-01 12:09:10 +0200

    only one priv key possible

--- app/routes/settings/comps/drtv-identity-keys-overview.js
+++ app/routes/settings/comps/drtv-identity-keys-overview.js
@@ -9,12 +9,13 @@ angular.module('cmRouteSettings').directive('cmIdentityKeysOverview', [
             controller: function ($scope) {
 
                 function refresh(){
-                    $scope.privateKeys  =   cmUserModel.loadLocalKeys() || [];
-                    $scope.publicKeys   =   cmUserModel.data.identity.keys || [];
-                    $scope.trustedKeys  =   $scope.publicKeys.filter(function(key){
-                                                return cmUserModel.trustsKey(key)
-                                            })
-                    $scope.signing      =   cmUserModel.state.is('signing')
+                    $scope.privateKeys  = cmUserModel.loadLocalKeys() || [];
+                    $scope.publicKeys   = cmUserModel.data.identity.keys || [];
+                    $scope.trustedKeys  = $scope.publicKeys.filter(function(key){
+                                                return cmUserModel.trustsKey(key);
+                                            });
+                    $scope.signing      = cmUserModel.state.is('signing');
+                    $scope.canCreate    = !cmUserModel.hasPrivateKey();
                 }
 
                 $scope.remove = function(key){
@@ -24,20 +25,22 @@ angular.module('cmRouteSettings').directive('cmIdentityKeysOverview', [
 
                 $scope.isTrustedKey = function(key){
                     return $scope.trustedKeys.indexOf(key) != -1
-                }
+                };
 
                 $scope.sortByPrivKeys = function(key) {
                     return !($scope.privateKeys.find(key) instanceof cmKey);
                 };
 
                 $scope.$on('$destroy', function(){
-                    cmUserModel.off('key:stored', refresh)
-                    cmUserModel.off('signature:saved', refresh)
-                })
+                    cmUserModel.off('key:stored', refresh);
+                    cmUserModel.off('signature:saved', refresh);
+                });
 
-                cmUserModel.state.on('change', refresh)
+                cmUserModel.state.on('change', refresh);
 
                 cmUserModel.on('key:stored', refresh);
+
+                cmUserModel.on('key:removed', refresh);
 
                 refresh()
             }
