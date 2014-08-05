2965e81bb33386069c0ece5add78f76cff992ac8
dutscher <dutscher@cameo.io>
2014-08-05 15:14:56 +0200

    update contact/identtiy key-level drtv

--- app/comps/contacts/drtv-key-level.js
+++ app/comps/contacts/drtv-key-level.js
@@ -1,20 +1,20 @@
 'use strict';
 
-angular.module('cmContacts')
-.directive('cmKeyLevel',[
+angular.module('cmContacts').directive('cmKeyLevel',[
     function (){
         return {
             restrict: 'E',
             template: '<cm-icons icons="cm-lock" alt="cm-unlock" count="{{count}}"></cm-icons>',
-            scope: {
-                keySize: '=cmData'
-            },
-            controller: function($scope){
-                $scope.count = 0;
+            controller: function($scope, $element, $attrs){
+                $scope.$watch($attrs.cmData, function(keySize){
+                    $scope.count = 0;
 
-                if($scope.keySize >= 2048){
-                    $scope.count = 2;
-                }
+                    if(keySize >= 2048){
+                        $scope.count = 2;
+                    }
+
+                    console.log(keySize, $scope.count)
+                });
             }
         }
     }
