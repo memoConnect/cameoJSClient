719e71a322edeaa85daf008db338e0e5544491af
dutscher <dutscher@cameo.io>
2014-08-25 19:15:26 +0200

    ignore autolink in drtv lastmessage only text

--- app/comps/ui/fltr-parse.js
+++ app/comps/ui/fltr-parse.js
@@ -4,13 +4,27 @@ angular.module('cmUi')
 .filter('cmParse', [
     '$filter',
     function($filter){
-        return function(html){
-            html = $filter('cmEscape')(html);
-            html = $filter('cmTranslate')(html);
-            html = $filter('cmInlineTranslate')(html);
-            html = $filter('cmAutolink')(html, 40);// $sce
-            html = $filter('cmEmoji')(html);
-            html = $filter('nl2br')(html);//$sce
+        return function(html, ignore){
+            if(ignore == undefined)
+                ignore = {};
+
+            if(!('escape' in ignore))
+                html = $filter('cmEscape')(html);
+
+            if(!('translate' in ignore))
+                html = $filter('cmTranslate')(html);
+
+            if(!('inlineTranslate' in ignore))
+                html = $filter('cmInlineTranslate')(html);
+
+            if(!('autolink' in ignore))
+                html = $filter('cmAutolink')(html, 40);// $sce
+
+            if(!('emoji' in ignore))
+                html = $filter('cmEmoji')(html);
+
+            if(!('nl2br' in ignore))
+                html = $filter('nl2br')(html);//$sce
             return html;
         }
     }
