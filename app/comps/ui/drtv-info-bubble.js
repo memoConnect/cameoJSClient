84b0b3532ef19a9a66250794880944c89bacbecb
dutscher <dutscher@cameo.io>
2014-10-06 18:34:30 +0200

    added en mixed i18n solved account clear at pw change prepared cm-info-bubble for watching on ngShow and jumpt to message

--- app/comps/ui/drtv-info-bubble.js
+++ app/comps/ui/drtv-info-bubble.js
@@ -1,32 +1,47 @@
 'use strict';
 
 angular.module('cmUi').directive('cmInfoBubble',[
-    function (){
+    '$document',
+    function ($document){
         return {
             restrict: 'AE',
             transclude: true,
-            scope:      {
-                            nosePos    :"="
-                        },
 
-            template:   '<div ng-transclude></div>'+
-                        '<i class="fa {{nose_icon}}"></i>',
+            template: '<div ng-transclude></div>' +
+                      '<i class="fa {{nose_icon}}"></i>',
 
-            link:       function(scope, element, attrs){
-                            scope.nose_icon = 'cm-nose-up'
+            link: function(scope, element, attrs){
+                scope.nose_icon = 'cm-nose-up';
 
-                            if(scope.nosePos == 'bottom')
-                                scope.nose_icon = 'cm-nose-down'
+                if(attrs.nosePos == 'bottom')
+                    scope.nose_icon = 'cm-nose-down';
 
-                            element.toggleClass('up',   scope.nose_icon == 'cm-nose-up')
-                            element.toggleClass('down', scope.nose_icon == 'cm-nose-down')
+                element.toggleClass('up',   scope.nose_icon == 'cm-nose-up');
+                element.toggleClass('down', scope.nose_icon == 'cm-nose-down');
 
-                            element.children('i.').css({
-                                top     : scope.nosePos == 'bottom' ? 'auto': '-1em',
-                                bottom  : scope.nosePos == 'bottom' ? '-1em' : 'auto',
-                                left    : attrs.noseX || '70%'
-                            })
-                        }
+                element.children('i.').css({
+                    top     : attrs.nosePos == 'bottom' ? 'auto': '-1em',
+                    bottom  : attrs.nosePos == 'bottom' ? '-1em' : 'auto',
+                    left    : attrs.noseX || '70%'
+                });
+
+                // TODO: jumpto real offset of cm-info-bubble
+
+//                scope.$watch(attrs.ngShow, function(bool) {
+//                    if (bool && bool != false) {
+//
+//                        var offset = element[0].getBoundingClientRect();
+//
+//                        console.log(offset)
+//
+//                        var bodyAndHtml = angular.element($document[0].querySelectorAll('body,html'));
+//                        angular.forEach(bodyAndHtml, function (tag) {
+//                            console.log(tag,offset.top)
+//                            tag.scrollTop = offset.top;
+//                        });
+//                    }
+//                });
+            }
         }
     }
 ]);
