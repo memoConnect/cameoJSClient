6e9ff5cd4c4a209ff4239b4e53eb7631eb7fed41
dutscher <dutscher@cameo.io>
2014-10-10 02:04:00 +0200

    fixxed terms

--- app/comps/ui/drtv-info-bubble.js
+++ app/comps/ui/drtv-info-bubble.js
@@ -20,7 +20,7 @@ angular.module('cmUi').directive('cmInfoBubble',[
                 element.toggleClass('down', scope.nose_icon == 'cm-nose-down');
 
                 element.children('i.').css({
-                    top     : attrs.nosePos == 'bottom' ? 'auto': '-1em',
+                    top     : attrs.nosePos == 'bottom' ? 'auto': '-3.5rem',
                     bottom  : attrs.nosePos == 'bottom' ? '-1em' : 'auto',
                     left    : attrs.noseX || '70%'
                 });
