3b4d9a9f7a08741d895418e08e5e852c17208228
dutscher <dutscher@cameo.io>
2014-10-16 18:41:00 +0200

    changed grunt tasks

--- grunt/app.js
+++ grunt/app.js
@@ -2,6 +2,7 @@ module.exports = function(grunt, options) {
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-template');
 
     grunt.registerTask('app:copy-core', [
         'clean:app-dist',
@@ -43,7 +44,30 @@ module.exports = function(grunt, options) {
                         }
                     ]
                 }
+            },
+            template: {
+                'app-files': {
+                    'options': {
+                        'data': {
+                            'currentVersion': options.globalCameoBuildConfig.config.version,
+                            'currentApiUrl': options.globalCameoBuildConfig.config.apiUrl,
+                            'autoLogin': options.globalCameoBuildConfig.config.autoLogin,
+                            'loadingBar': options.globalCameoBuildConfig.config.loadingBar,
+                            'enableDebug': options.globalCameoBuildConfig.config.enableDebug,
+                            'performancePage': options.globalCameoBuildConfig.config.performancePage,
+                            'phonegapFiles': options.globalCameoBuildConfig.debug.weinre ? '<script src="http://' + options.globalCameoBuildConfig.debug.weinreIp + ':8080/target/target-script-min.js#anonymous"></script>' : '',
+                            'phonegapOnload': ''
+                        }
+                    },
+                    'files': {
+                        'dist/app/index.html': ['app/index.html'],
+                        'dist/app/icons.html': ['core/icons.html'],
+                        'build/app/base/config.js': ['app/base/config.js'],
+                        // performance page
+                        'dist/app/performance.html': ['core/performance.html']
+                    }
+                }
             }
         }
     }
-}
+};
