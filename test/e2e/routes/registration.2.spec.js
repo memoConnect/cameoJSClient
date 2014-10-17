306a3257cc91ea1c0c8d537b29eb13dac2341321
Empujador <empu@cameo.io>
2014-10-17 15:08:52 +0200

    update cameoid min length

--- test/e2e/routes/registration.2.spec.js
+++ test/e2e/routes/registration.2.spec.js
@@ -30,12 +30,12 @@ describe('Registration: ', function () {
         util.get("/registration");
         util.waitForPageLoad('/registration')
 
-        $("[data-qa='input-cameoId']").sendKeys("moep")
+        $("[data-qa='input-cameoId']").sendKeys("mo")
         ptor.sleep(1001)//adaptive change delay
         util.checkWarning("cameoId-info-user-min-letter-count")
 
         // it should disappear if we type more letters
-        $("[data-qa='input-cameoId']").sendKeys("moep")
+        $("[data-qa='input-cameoId']").sendKeys("ep")
         ptor.sleep(1001)//adaptive change delay
         expect($("[data-qa='cameoId-info-user-min-letter-count']").isDisplayed()).toBe(false)
 
