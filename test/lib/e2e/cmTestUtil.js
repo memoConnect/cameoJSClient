af8971bcc7306c44ef86c9161d2959aeb33c4a0d
dutscher <dutscher@cameo.io>
2014-10-21 10:01:22 +0200

    longer timeout for waitingTimeoutes

--- test/lib/e2e/cmTestUtil.js
+++ test/lib/e2e/cmTestUtil.js
@@ -142,7 +142,7 @@ this.login = function (username, password, expectedRoute) {
     return this
 }
 
-this.createTestUser = function (testUserId, from) {
+this.createTestUser = function (testUserId, from){
     //console.log('from ->' + from)
 
     this.logout()
