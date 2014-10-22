45f1d0215861bbe4d15b39225a910a533ed39fe5
dutscher <dutscher@cameo.io>
2014-10-22 16:28:35 +0200

    solved cm-footer watcher

--- desktop/comps/ui/drtv-column.js
+++ desktop/comps/ui/drtv-column.js
@@ -1,14 +1,31 @@
 'use strict';
 
 angular.module('cmDesktopUi').directive('cmColumn',[
-    function () {
+    '$rootScope', '$timeout',
+    function ($rootScope, $timeout) {
         return {
             restrict: 'E',
             link: function(scope, element){
-                if(element.find('cm-footer').length == 0)
-                    element.addClass('without-footer')
-                else
-                    element.removeClass('without-footer')
+                function checkFooter(){
+                    console.log(element.find('cm-footer'))
+                    if(element.find('cm-footer').length == 0)
+                        element.addClass('without-footer')
+                    else
+                        element.removeClass('without-footer')
+                }
+
+                checkFooter();
+
+                var watchersEnd = $rootScope.$on('cmFooter:stateChanged',function(){
+                    console.log('footer wech');
+                    $timeout(function(){
+                        checkFooter();
+                    },50);
+                });
+
+                scope.$on('$destroy', function(){
+                    watchersEnd();
+                });
             }
         }
     }
