418cc7e786034739680db1ddb561e5ad3ef741db
Michael Merz <dermicha@cameo.io>
2014-10-23 18:39:55 +0200

    OSX IconSet + DMG Image

--- grunt/deploy.js
+++ grunt/deploy.js
@@ -5,6 +5,7 @@ module.exports = function (grunt, options) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-node-webkit-builder');
+    grunt.loadNpmTasks('grunt-appdmg');
 
     grunt.registerTask('app:deploy', [
         'app:deploy-without-template',
@@ -24,7 +25,9 @@ module.exports = function (grunt, options) {
         'clean:nodeWebkit',
         'copy:nwDeploy',
         'template:nodeWebkitPackage',
-        'nodewebkit'
+        'nodewebkit',
+        'copy:nwIconSet',
+        'appdmg'
     ]);
 
     return {
@@ -82,6 +85,10 @@ module.exports = function (grunt, options) {
                         }
                     ]
 
+                },
+                nwIconSet: {
+                    src: "resource/iconSet/cameoNet.icns",
+                    dest: "build/nodeWebkit/"+options.globalCameoBuildConfig.phonegap.baseName+"/osx/"+options.globalCameoBuildConfig.phonegap.baseName+".app/Contents/Resources/nw.icns"
                 }
             },
             nodewebkit: {
@@ -95,12 +102,29 @@ module.exports = function (grunt, options) {
                 'nodeWebkitPackage': {
                     'options': {
                         'data': {
-                            'currentVersion': options.globalCameoBuildConfig.config.version
+                            'currentVersion': options.globalCameoBuildConfig.config.version,
+                            'appBaseName' : options.globalCameoBuildConfig.phonegap.baseName
                         }
                     },
                     'files': {
                         'dist/nodeWebkit/package.json': ['resource/templates/nodeWebkit/package.json']
                     }
+                }
+            },
+            appdmg: {
+                options: {
+                    title: 'cameoNet for MacOSX',
+                    icon: 'resource/iconSet/cameoNet.icns',
+                    background: 'resource/iconSet/background.png',
+                    "icon-size": 80,
+                    contents: [
+                        {x: 448, y: 344, type: 'link', path: '/Applications'},
+                        {x: 192, y: 344, type: 'file', path: "build/nodeWebkit/"+options.globalCameoBuildConfig.phonegap.baseName+"/osx/"+options.globalCameoBuildConfig.phonegap.baseName+".app"}
+                        <!-- {x: 512, y: 128, type: 'file', path: 'path/to/extra-file.txt'}-->
+                    ]
+                },
+                target: {
+                    dest: 'build/nodeWebkit/'+options.globalCameoBuildConfig.phonegap.baseName+'-'+options.globalCameoBuildConfig.config.version+'.dmg'
                 }
             }
 
