c013b703cb174cf9c69134a065af2c99ed7241a6
Empujador <empu@cameo.io>
2014-10-27 12:46:58 +0100

    update test, add externalUser detail updates

--- test/e2e/routes/contact/contact.1.spec.js
+++ test/e2e/routes/contact/contact.1.spec.js
@@ -2,10 +2,18 @@ var config = require("../../config-e2e-tests.js")
 var util = require("../../../lib/e2e/cmTestUtil.js")
 
 describe('Route Contact: ', function () {
-    var ptor = util.getPtorInstance()
+    var ptor = util.getPtorInstance(),
+        extUserName = 'moeper_'+ Date.now(),
+        extUserTel = '+4912345678',
+        extUserMail1 = 'mail@moeper.de',
+        extUserMail2 = 'moep@moeper.de',
+        testUser
+
+    it('should create a test user', function(){
+        testUser = util.createTestUser(undefined,'route contact')
+    })
 
     it('at first goto "#/contact/list".', function(){
-        util.login()
         util.get('/contact/list')
         util.expectCurrentUrl('#/contact/list')
     })
@@ -15,7 +23,7 @@ describe('Route Contact: ', function () {
         expect($('cm-contact-list').isPresent()).toBe(true)
     })
 
-    it('contacts should exists in list', function(){
+    it('contacts should exists in list (min Support User)', function(){
         util.waitForElement('cm-contact-tag');
         var tags = $$('cm-contact-list cm-contact-tag')
         tags.count().then(function(count){
@@ -55,7 +63,6 @@ describe('Route Contact: ', function () {
     })
 
     describe('create external contact', function(){
-        var userName = 'moeper_'+ Date.now();
 
         it('open modal and click create new contact',function(){
             util.expectCurrentUrl('#/contact/list')
@@ -78,9 +85,12 @@ describe('Route Contact: ', function () {
         })
 
         it('create external contact', function(){
-            $("[data-qa='input-displayname']").sendKeys(userName)
-            $("[data-qa='input-phonenumber']").sendKeys('12345')
-            $("[data-qa='input-email']").sendKeys('die@gibts.net')
+            //$("[data-qa='input-displayname']").sendKeys(extUserName)
+            util.setVal('input-displayname', extUserName)
+            //$("[data-qa='input-phonenumber']").sendKeys(extUserTel)
+            util.setVal('input-phonenumber', extUserTel)
+            //$("[data-qa='input-email']").sendKeys(extUserMail1)
+            util.setVal('input-email', extUserMail1)
 
             $('cm-footer button').click()
         })
@@ -89,9 +99,97 @@ describe('Route Contact: ', function () {
             util.waitForPageLoad('/contact')
             util.waitForElement('cm-contact-tag')
 
-            util.headerSearchInList(userName)
+            util.headerSearchInList(extUserName)
 
             expect($$('cm-contact-list cm-contact-tag cm-avatar').count()).toBe(1)
         })
+
+        it('should find external user after logout/login (1)', function(){
+            util.login(testUser, 'password');
+
+            util.get('/contact')
+            util.waitForPageLoad('/contact')
+
+            util.headerSearchInList(extUserName)
+            $$('cm-contact-list cm-contact-tag cm-avatar').first().click()
+            util.expectCurrentUrl('#/contact/.*')
+        })
+
+        it('should be the same details in contact (1)', function(){
+            util.waitForQa('input-displayname');
+            util.getVal('input-displayname').then(function(value){
+                expect(value).toBe(extUserName)
+            })
+
+            util.waitForQa('input-phonenumber');
+            util.getVal('input-phonenumber').then(function(value){
+                expect(value).toBe(extUserTel)
+            })
+
+            util.waitForQa('input-email');
+            util.getVal('input-email').then(function(value){
+                expect(value).toBe(extUserMail1)
+            })
+        })
+
+        it('should update external User', function(){
+            util.clearInput('input-phonenumber')
+
+            util.clearInput('input-email')
+            util.setVal('input-email', extUserMail2)
+
+            $('cm-footer button').click()
+
+            util.waitAndCloseNotify();
+        })
+
+        it('should be the same details in contact after updating', function(){
+            util.waitForQa('input-displayname');
+            util.getVal('input-displayname').then(function(value){
+                expect(value).toBe(extUserName)
+            })
+
+            util.waitForQa('input-phonenumber');
+            util.getVal('input-phonenumber').then(function(value){
+                expect(value).toBe('')
+            })
+
+            util.waitForQa('input-email');
+            util.getVal('input-email').then(function(value){
+                expect(value).toBe(extUserMail2)
+            })
+        })
+
+        it('should find external user after logout/login (2)', function(){
+            util.login(testUser, 'password');
+
+            util.get('/contact')
+            util.waitForPageLoad('/contact')
+
+            util.headerSearchInList(extUserName)
+            $$('cm-contact-list cm-contact-tag cm-avatar').first().click()
+            util.expectCurrentUrl('#/contact/.*')
+        })
+
+        it('should be the same details in contact (2)', function(){
+            util.waitForQa('input-displayname');
+            util.getVal('input-displayname').then(function(value){
+                expect(value).toBe(extUserName)
+            })
+
+            util.waitForQa('input-phonenumber');
+            util.getVal('input-phonenumber').then(function(value){
+                expect(value).toBe('')
+            })
+
+            util.waitForQa('input-email');
+            util.getVal('input-email').then(function(value){
+                expect(value).toBe(extUserMail2)
+            })
+        })
+    })
+
+    it('delete test user', function(){
+        util.deleteTestUser(testUser)
     })
 })
