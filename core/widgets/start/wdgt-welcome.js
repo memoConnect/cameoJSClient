2da5fb4799fe3968a08700e07c069dd9b1036cd9
Empujador <empu@cameo.io>
2014-10-28 12:21:51 +0100

    update text in welcome widget update text in quickstart widget

--- core/widgets/start/wdgt-welcome.js
+++ core/widgets/start/wdgt-welcome.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('cmWidgets').directive('cmWidgetWelcome', [
-    function(){
+    '$rootScope',
+    function($rootScope){
         return {
             restrict: 'E',
             templateUrl: 'widgets/start/wdgt-welcome.html',
             controller: function ($scope) {
+                $scope.generateKey = function(){
+                    $rootScope.generateAutomatic = {
+                        generate:true,
+                        keySize: 2048
+                    };
+
+                    $rootScope.goTo('/settings/identity/key/create');
+                }
             }
         }
     }
