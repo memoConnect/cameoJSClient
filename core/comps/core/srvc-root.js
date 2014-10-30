05341334ae83c124dfcc6ae2aaf991a0d9b0f3d0
Empujador <empu@cameo.io>
2014-10-30 17:05:04 +0100

    rm console.log at route change

--- core/comps/core/srvc-root.js
+++ core/comps/core/srvc-root.js
@@ -19,7 +19,7 @@ angular.module('cmCore').service('cmRootService', [
          * @param replace {boolean}
          */
         $rootScope.goTo = function(path, replace){
-            cmLogger.debug('cmRootService.goTo ' + path);
+            //cmLogger.debug('cmRootService.goTo ' + path);
 
             path = path[0] == '/' ? path : '/'+path;
             if(cmJob.isActive() !== false){
