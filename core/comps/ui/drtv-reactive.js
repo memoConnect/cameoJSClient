084d62ccf6cc589d304c72a9665cc5adcbed1fd5
Empujador <empu@cameo.io>
2014-11-10 10:24:42 +0100

    add cm-reactive to header items in app routes

--- core/comps/ui/drtv-reactive.js
+++ core/comps/ui/drtv-reactive.js
@@ -1,16 +1,74 @@
 'use strict';
 
 angular.module('cmUi').directive('cmReactive',[
-    function (){
+    '$timeout',
+    '$parse',
+    function ($timeout,$parse){
         return {
             restrict: 'A',
-            controller: function($scope, $element){
-                console.log($element)
-                $element.on('click',function(){
+            link: function(scope, element, attrs){
+                var isTouch = false;
 
-                })
+                function isValidTouch(evt){
 
+                    function getOffsetSum(elem) {
+                        var top=0, left=0;
+                        while(elem) {
+                            top = top + parseInt(elem.offsetTop);
+                            left = left + parseInt(elem.offsetLeft);
+                            elem = elem.offsetParent;
+                        }
+                        return {top: top, left: left};
+                    }
 
+                    var offset = getOffsetSum(element[0]);
+
+                    if(evt.changedTouches[0].pageY <= (offset.top + element[0].offsetHeight) && evt.changedTouches[0].pageY > offset.top
+                        && evt.changedTouches[0].pageX <= (offset.left + element[0].offsetWidth) && evt.changedTouches[0].pageX > offset.left
+                    ){
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                element.on('mouseenter', function(){
+                    element.addClass('is-hover');
+                });
+
+                element.on('mouseleave', function(){
+                    element.removeClass('is-hover');
+                });
+
+                element.on('click',function(){
+                    if(!isTouch){
+                        element.addClass('is-clicked');
+                        $timeout(function(){element.removeClass('is-clicked')},250)
+                    }
+                });
+
+                element.on('touchstart',function(){
+                    isTouch = true;
+                    element.removeClass('is-hover');
+                });
+
+                element.on('touchend',function(evt){
+                    evt.preventDefault();
+
+                    element.removeClass('is-hover');
+
+                    if(isValidTouch(evt)){
+                        element.addClass('is-touched');
+                        $timeout(function(){element.removeClass('is-touched')},250)
+                    }
+
+                    if('ngClick' in attrs){
+                        var fn = $parse(attrs['ngClick']);
+                        fn(scope,{$event:evt});
+                    }
+
+                    isTouch = false;
+                });
             }
         }
     }
