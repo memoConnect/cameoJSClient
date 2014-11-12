69678c22b381ad55957e5f0fced482494eff1cbc
dutscher <dutscher@cameo.io>
2014-11-12 19:42:05 +0100

    uncomment prepurl app launcher

--- app/routes/prepurl/prepurl-ctrl.js
+++ app/routes/prepurl/prepurl-ctrl.js
@@ -1,19 +1,33 @@
 'use strict';
 
 angular.module('cmRoutes')
-.controller('PrepurlCtrl',
+.controller('PrepurlCtrl', [
+    'cmDevice', 'cmConfig',
+    '$routeParams', '$rootScope', '$scope', '$timeout',
     function(cmDevice, cmConfig,
-             $routeParams, $rootScope) {
+             $routeParams, $rootScope, $scope, $timeout) {
 
-        // on mobile browser call app otherwise go to purl
-        if (!cmDevice.isApp() && cmDevice.isMobile('PrepurlCtrl')) {
-            setTimeout(function(){
-                $rootScope.goTo('/purl/'+$routeParams.purlId);
-            }, 25);
-            window.location = cmConfig.appProtocol+'://?purlId='+$routeParams.purlId;
-        // otherwise
-        } else {
-            $rootScope.goTo('/purl/'+$routeParams.purlId);
-        }
+        var params = angular.extend({},$routeParams);
+
+        $rootScope.goTo('/purl/' + params.purlId);
+        return false;
+
+        // TODO: ???!!!
+
+        $scope.$on('$routeChangeSuccess', function() {
+            $scope.launchSrc = '';
+            // on mobile browser call app otherwise go to purl
+            if (!cmDevice.isApp() && cmDevice.isMobile('PrepurlCtrl')) {
+                $timeout(function () {
+                    $rootScope.goTo('/purl/'+params.purlId);
+                }, 25);
+                //window.location =
+                $scope.launchSrc = cmConfig.appProtocol + '://?purlId=' + params.purlId;
+                //<iframe ng-src="{{launchSrc}}"></iframe>
+                // otherwise
+            } else {
+                $rootScope.goTo('/purl/' + params.purlId);
+            }
+        });
     }
-);
+]);
