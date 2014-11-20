bab3a86fa0d781db4ba635079a662b8b5fa3cdf0
Empujador <empu@cameo.io>
2014-11-20 14:33:47 +0100

    turn off debug modus

--- core/comps/phonegap/srvc-device.js
+++ core/comps/phonegap/srvc-device.js
@@ -11,7 +11,7 @@ angular.module('cmPhonegap')
 
         var self = {
             plugin: null,
-            emulateDevice: true,
+            emulateDevice: false,
             emulateDeviceType: 'android', // android | ios | winphone
             flags: {},
 
