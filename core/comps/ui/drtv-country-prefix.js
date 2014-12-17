c0c2d0ce0662345492ed8ee8e98ae0fec3c298ab
dutscher <dutscher@cameo.io>
2014-12-17 17:24:23 +0100

    solved json compartion and icons.html addition

--- core/comps/ui/drtv-country-prefix.js
+++ core/comps/ui/drtv-country-prefix.js
@@ -5,43 +5,66 @@ angular.module('cmUi')
     'cmCountryPhonePrefix', 'flags',
     function (cmCountryPhonePrefix, flags){
 
-        cmCountryPhonePrefix
-        .loadPrefixes()
-        .then(function(prefixes){
-            console.log(flags.items,prefixes)
-        });
-
         return{
             restrict: 'E',
-            link: function(scope, element){
+            template: '<div ng-repeat="country in countries">' +
+                        '<span ng-bind-html="::\':\'+country.shortcut+\':\'|flags"></span>'+
+                        '+{{country.numberPrefix}}'+
+                      '</div>',
+            controller: function($scope){
 
+                $scope.countries = [];
+
+                function init(){
+                    $scope.countries = cmCountryPhonePrefix.countries;
+                }
+
+                if(!cmCountryPhonePrefix.ready) {
+                    cmCountryPhonePrefix.on('cmCountryPhonePrefix:loaded', init);
+
+                    $scope.$on('$destroy', function () {
+                        cmCountryPhonePrefix.off('cmCountryPhonePrefix:loaded', init);
+                    });
+                } else {
+                    init();
+                }
             }
         }
     }
 ])
 .service('cmCountryPhonePrefix', [
+    'cmObject',
     '$http',
-    function($http){
+    function(cmObject,
+             $http){
         var self = {
-            prefixes: [],
+            countries: [],
+            ready: false,
             loadPrefixes: function(){
-
-                if(self.prefixes.length > 0){
+                if(self.countries.length > 0){
                     return false;
                 }
 
+                // load
                 return $http({
                     url:'i18n/countryphoneprefix.json',
                     method:'GET'
                 }).then(
                     function(response){
-                        console.log(response)
+                        if(typeof response.data == 'object') {
 
-                        var structure = "country,numberPrefix"
+                            response.data.forEach(function(rawCountry) {
+                                var country = rawCountry.split(',');
 
-                        self.prefixes = response.data;
+                                self.countries.push({
+                                    shortcut:country[0],
+                                    numberPrefix:country[1]
+                                });
+                            });
 
-                        return response.data;
+                            self.ready = true;
+                            self.trigger('cmCountryPhonePrefix:loaded');
+                        }
                     },
                     function(response){
 
@@ -49,6 +72,11 @@ angular.module('cmUi')
                 );
             }
         };
+
+        cmObject.addEventHandlingTo(self);
+
+        self.loadPrefixes();
+
         return self;
     }
 ]);
