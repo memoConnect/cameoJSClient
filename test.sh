22bf6c2b94aa2cbd7d961a8badb8ddca5a226eff
Empujador <empu@cameo.io>
2014-12-18 10:36:58 +0100

    check test.sh

--- test.sh
+++ test.sh
@@ -28,6 +28,8 @@ fi
 ./setup.sh
 
 echo -e "\e[33m[ CameoClient - running unit/e2e tests ]\033[0m"
+echo ${apiUrlArg}
+
 ./node_modules/grunt-cli/bin/grunt tests-unit ${target} ${wwwUrlArg} ${apiUrlArg}
 ./node_modules/grunt-cli/bin/grunt tests-e2e  ${target} ${wwwUrlArg} ${apiUrlArg}
 
