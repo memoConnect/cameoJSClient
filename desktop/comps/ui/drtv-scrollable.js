7904b9f53b606d84dfed53de12a76467120c6893
Empujador <empu@cameo.io>
2014-12-19 10:57:08 +0100

    finish height calculation of cm-scrollable

--- desktop/comps/ui/drtv-scrollable.js
+++ desktop/comps/ui/drtv-scrollable.js
@@ -8,20 +8,6 @@ angular.module('cmDesktopUi').directive('cmScrollable',[
             link: function (scope, element) {
                 $rootScope.$emit('cm-scrollable:loaded')
 
-                /**
-                 * Function returns a reference of requested parent element.
-                 * @param {String} tag_name Tag name of requested parent element.
-                 * @param {HTMLElement} el Initial element (from which search begins).
-                 */
-                function findParent (tag_name, el) {
-                    // loop up until parent element is found
-                    while (el && el.nodeName !== tag_name) {
-                        el = el.parentNode;
-                    }
-                    // return found element
-                    return el;
-                }
-
                 function getOffsetSum(elem) {
                     var top=0, left=0;
                     while(elem) {
@@ -33,14 +19,8 @@ angular.module('cmDesktopUi').directive('cmScrollable',[
                 }
 
                 function resize(){
-                    var column = findParent('CM-COLUMN',element[0]);
-                    console.log('column', column)
                     var offset = getOffsetSum(element[0])
-                    console.log('offset', offset.top)
-
-
-                    //var newHeight = ($window.innerHeight - ($window.document.querySelector('cm-header').offsetHeight * 2) - $window.document.querySelector('cm-footer').offsetHeight);
-                    var newHeight = ($window.innerHeight - (offset.top - $window.document.querySelector('cm-footer').offsetHeight));
+                    var newHeight = ($window.innerHeight - offset.top - $window.document.querySelector('cm-footer').offsetHeight) - 20; // scrollbar
                     element.css({'height':newHeight + 'px'})
                 }
 
