54849d7d3bbe898b787acd4df3354bdd7b783851
dutscher <dutscher@cameo.io>
2014-12-23 17:45:36 +0100

    fixxed spec

--- test/unit/comps/validate/validate-phone-drtv.spec.js
+++ test/unit/comps/validate/validate-phone-drtv.spec.js
@@ -5,21 +5,11 @@ describe("Directive cmValidatePhone", function(){
     var element,
         form,
         input,
-        scope,
-        $httpBackend
+        scope
 
-    beforeEach(module('cmCore',[
-        'cmApiProvider',
-        function(cmApiProvider){
-            cmApiProvider
-                .setWithoutApiUrl()
-        }
-    ]))
     beforeEach(module('cmConfig'))
     beforeEach(module('cmValidate'))
-    beforeEach(inject(function($compile, $rootScope, _$httpBackend_){
-        $httpBackend = _$httpBackend_
-
+    beforeEach(inject(function($compile, $rootScope){
         scope = $rootScope.$new()
         element = angular.element('<form name="form"><input name="phone" cm-validate-phone ng-model="phone" /></form>')
 
@@ -43,12 +33,14 @@ describe("Directive cmValidatePhone", function(){
 
     it('should be valid', function(){
         scope.phone = '123456'
+        scope.$apply()
         expect(form.phone.$valid).toBe(true)
         expect(form.phone.$invalid).toBe(false)
     })
 
     it('should be invalid', function(){
         scope.phone = 'asd'
+        scope.$apply()
         expect(form.phone.$valid).toBe(false)
         expect(form.phone.$invalid).toBe(true)
     })
