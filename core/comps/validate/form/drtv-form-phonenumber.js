3c51acff4767a7e87491ab96f5d669977943439b
dutscher <dutscher@cameo.io>
2015-02-02 12:13:28 +0100

    fixxed errorCodes at srtv email & phone

--- core/comps/validate/form/drtv-form-phonenumber.js
+++ core/comps/validate/form/drtv-form-phonenumber.js
@@ -36,7 +36,7 @@ angular.module('cmValidate')
 
                 var killWatcher = $rootScope.$on('cmValidate:error', function(event, errorCodes){
                     if(errorCodes.length > 0 && errorCodes.indexOf('PHONENUMBER_INVALID') >= 0) {
-                        $scope.cmInnerForm.phoneNumberDisp.$setValidity('phoneNumberDisp', false);
+                        $scope.cmInnerForm.phoneNumberDisp.$setValidity('invalid', false);
                     }
                 });
 
