24a4c031c1356313168d748efd7414ab9da83353
reimerei <github@bjrm.de>
2015-02-05 18:22:16 +0100

    added testScreenshotPath to test script

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+testScreenShotBase="/opt/screenshots"
+
 case "$1" in
    "stage")
         target=--target="stage"
+        testScreenshotPath="${testScreenShotBase}/stage"
       ;;
    "dev")
         target=--target="dev"
+        testScreenshotPath="${testScreenShotBase}/dev"
       ;;
    "test")
         target=--target="test"
+        testScreenshotPath="${testScreenShotBase}/test"
       ;;
    "prod")
         target=--target="prod"
+        testScreenshotPath="${testScreenShotBase}/prod"
       ;;
 esac
 
@@ -29,10 +35,10 @@ fi
 
 if [ ! -z "$4" ]; then
     echo -e "\e[33m[ CameoClientTest running e2e specs: $4 ]\033[0m"
-    ./node_modules/grunt-cli/bin/grunt tests-e2e:all  ${target} ${wwwUrlArg} ${apiUrlArg} --specs=${4} --disableUglify
+    ./node_modules/grunt-cli/bin/grunt tests-e2e:all  ${target} ${wwwUrlArg} ${apiUrlArg} --specs=${4} --disableUglify --testScreenshotPath="${testScreenShotBase}/custom"
 else
     echo -e "\e[33m[ CameoClientTest - running all unit and e2e tests ]\033[0m"
     ./node_modules/grunt-cli/bin/grunt tests-unit ${target} ${wwwUrlArg} ${apiUrlArg}
     [ $? -eq 0 ] || exit $?; # exit for none-zero return code
-    ./node_modules/grunt-cli/bin/grunt tests-e2e  ${target} ${wwwUrlArg} ${apiUrlArg}
+    ./node_modules/grunt-cli/bin/grunt tests-e2e  ${target} ${wwwUrlArg} ${apiUrlArg} --testScreenShotPath=${testScreenshotPath}
 fi
