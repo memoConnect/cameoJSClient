a22e07aa67254fd606bd6a6735749414babe4080
dutscher <dutscher@cameo.io>
2015-02-12 11:28:17 +0100

    zs

--- resource/browserPlugin/src/common/content.js
+++ resource/browserPlugin/src/common/content.js
@@ -1,8 +1,8 @@
 var browserName = kango.browser.getName();
 
 function callApp(json,eventName){
-	var event = new CustomEvent(eventName, {'detail':json||{}});
-	window.dispatchEvent(event);
+    var event = new CustomEvent(eventName, {'detail':json||{}});
+    window.dispatchEvent(event);
 }
 
 kango.console.log('plugin cameoNet content '+browserName)
@@ -11,42 +11,42 @@ callApp({},'cmBrowser:isAvailable');
 
 // app check if browser plugin is available
 window.addEventListener('cmBrowser:checkAvailability', function(){
-	callApp({},'cmBrowser:isAvailable');
+    callApp({},'cmBrowser:isAvailable');
 });
 
 // set
 window.addEventListener('cmBrowser:setItem', function(event){
-	var params = event.detail;
-	kango.storage.setItem(params.key,params.value);
-	callApp({},'cmBrowser:setItemDone');
+    var params = event.detail;
+    kango.storage.setItem(params.key,params.value);
+    callApp({},'cmBrowser:setItemDone');
 });
 
 // get
 window.addEventListener('cmBrowser:getItem', function(event){
-	var params = event.detail,
-		returnObj = {
-			value: kango.storage.getItem(params.key)
-		};
-	callApp(returnObj,'cmBrowser:setGetDone');
+    var params = event.detail,
+        returnObj = {
+            value: kango.storage.getItem(params.key)
+        };
+    callApp(returnObj,'cmBrowser:setGetDone');
 });
 
 // remove
 window.addEventListener('cmBrowser:removeItem', function(event){
-	var params = event.detail;
-	kango.storage.removeItem(params.key);
-	callApp({},'cmBrowser:removeItemDone');
+    var params = event.detail;
+    kango.storage.removeItem(params.key);
+    callApp({},'cmBrowser:removeItemDone');
 });
 
 // get all
 window.addEventListener('cmBrowser:getAllKeys', function(){
-	var returnObj = {
+    var returnObj = {
         keys: kango.storage.getKeys()
     };
-	callApp(returnObj,'cmBrowser:getAllKeysDone');
+    callApp(returnObj,'cmBrowser:getAllKeysDone');
 });
 
 // clear storage
 window.addEventListener('cmBrowser:clear', function(){
-	kango.storage.clear();
-	callApp({},'cmBrowser:clearDone');
+    kango.storage.clear();
+    callApp({},'cmBrowser:clearDone');
 });
