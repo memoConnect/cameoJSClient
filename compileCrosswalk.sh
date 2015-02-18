4f1ee2040f8e771e8ba1538932e9ecb2295401b1
reimerei <github@bjrm.de>
2015-02-18 12:14:53 +0100

    read location of crosswalk directory from enviroment variable

--- compileCrosswalk.sh
+++ compileCrosswalk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-crosswalkDir="/opt/crosswalk-cordova-10.39.235.15-arm"
 cordovaVersion="3.6.3-0.2.13"
 phongapVersion="3.6.3-0.22.7"
 antProperties="ant.properties"
@@ -8,17 +7,21 @@ antProperties="ant.properties"
 [ $(cordova --version) == ${cordovaVersion} ]
 [ $(phonegap --version) == ${phongapVersion} ]
 
+[ ! -z ${ANDROID_HOME} ]
+[ ! -z ${JAVA_HOME} ]
+[ ! -z ${CROSSWALK_HOME} ]
+
 cd build/phonegap-tmp
 cordova platform rm android
 cordova platform add android
 
 cd platforms/android/
 sed -i '/<\/application>/a <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" \/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" \/>' AndroidManifest.xml
-cp ${crosswalkDir}/VERSION .
+cp ${CROSSWALK_HOME}/VERSION .
 
 cd CordovaLib/
 rm -rf *
-cp -a ${crosswalkDir}/framework/* .
+cp -a ${CROSSWALK_HOME}/framework/* .
 android update project --subprojects --path . --target "android-19"
 ant debug
 
